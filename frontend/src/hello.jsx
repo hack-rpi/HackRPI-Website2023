a9130697d14a2db6ea55021e491840adc9e088ed
Jonathan Kloepfer <kloepj@rpi.edu>
2023-02-07 22:30:42 -0500

    Start

---
+++ frontend/src/hello.jsx
@@ -0,0 +1,4 @@
+
+const Hello = () => <h1>Hello from React!</h1>;
+
+export default Hello;
---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Hello from "./hello";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Hello />);
