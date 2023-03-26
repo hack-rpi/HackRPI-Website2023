9be160dce1cc332e6f7243422e14a10e26d0daae
Jonathan Kloepfer <kloepj@rpi.edu>
2023-03-25 22:35:32 -0400

    Hopefully everything fixed

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output : {
     path: path.resolve(__dirname,'build'),
     filename: '[name][contenthash].js',
-    publicPath: 'http:://hackrpi.com',
+    publicPath: 'hackrpi.com',
     assetModuleFilename:'[name][ext]',
     clean: true,
   },
