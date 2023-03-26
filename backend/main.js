0ab4390e20f93df45d7f09b0ba755b7bf8aa3fee
Jonathan Kloepfer <kloepj@rpi.edu>
2023-03-25 22:57:36 -0400

    Debug work

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 // serve up production assets
-app.use(express.static('../build'));
+//app.use(express.static('/build'));
 // let the react app to handle any unknown routes
 // serve up the index.html if express does'nt recognize the route
 const path = require('path');
