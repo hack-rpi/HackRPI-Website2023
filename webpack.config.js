fd0b14a9d8c2516b2a99a5931c4d5e6c19a4e53a
Jonathan Kloepfer <kloepj@rpi.edu>
2023-03-25 22:25:26 -0400

    More release Work

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output : {
     path: path.resolve(__dirname,'build'),
     filename: '[name][contenthash].js',
-    publicPath: 'bulid/',
+    publicPath: 'http:://hackrpi.com',
     assetModuleFilename:'[name][ext]',
     clean: true,
   },
