14335becaf664ac92c82c14b2a7d0795780c862a
Jonathan Kloepfer <kloepj@rpi.edu>
2023-03-25 23:09:35 -0400

    more Fix

--- backend/main.js
+++ backend/main.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 // serve up production assets
-//app.use(express.static('/build'));
+app.use(express.static(__dirname+"/.."+'/build'));
 // let the react app to handle any unknown routes
 // serve up the index.html if express does'nt recognize the route
 const path = require('path');
 console.log(path);
-app.get('*', (req, res) => {
+app.get('', (req, res) => {
 console.log(req)
 res.sendFile(path.resolve(__dirname+"/..", 'build', req.route.path));
 });
