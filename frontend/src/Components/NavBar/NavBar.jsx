77fa1aa8bfaa88817ffc24f21626a7f8d5242f08
Jonathan Kloepfer <kloepj@rpi.edu>
2023-06-30 14:41:18 -0400

    More fields

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -12,92 +12,109 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Select from 'react-select';
 export default function NavBar() {
-  const [show, setShow] = useState(false);
-  const [selectedOption, setSelectedOption] = useState('School');
-  const [selectedOption2, setSelectedOption2] = useState('Country');
-  console.log(data2);
-  return (
-    <>
-      <Modal
-        show={show}
-        onHide={() => setShow(false)}
-        dialogClassName="modal-90w"
-        aria-labelledby="example-custom-modal-styling-title"
-      >
-        <Modal.Body>
-          <Form>
-            <Form.Control type="text" placeholder="First Name" required />
-            <br />
-            <Form.Control type="text" placeholder="Last Name" />
-            <br />
-            <Form.Control type="text" placeholder="Email" />
-            <br />
-            <Form.Control type="text" placeholder="Phone Number" />
-            <br />
-            <Form.Select aria-label="Gender">
-              <option value="Unkown">Prefer Not Say</option>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-              <option value="Non-binary">Non-binary</option>
-            </Form.Select>
-            <br />
-            <Select
-              defaultValue={selectedOption}
-              onChange={setSelectedOption}
-              options={data}
-            />
-            <br />
-            <Select
-              defaultValue={selectedOption2}
-              onChange={setSelectedOption2}
-              options={data2}
-            />
-          </Form>
-        </Modal.Body>
-      </Modal>
-      <Navbar
-        style={{ backgroundColor: '#8B0000', padding: '1rem' }}
-        variant="dark"
-        sticky="top"
-      >
-        <Container fluid>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              fontSize: '1.25rem',
-            }}
-          >
-            <Navbar.Brand href="#home">
-              <img
-                src={logo}
-                width="35"
-                height="32"
-                className="d-inline-block align-top"
-              />
-              HackRPI X
-            </Navbar.Brand>
-            <Nav.Link
-              href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
-              style={{
-                paddingLeft: '1rem',
-                color: 'white',
-                textDecoration: 'none',
-              }}
-            >
-              MLH Code of Conduct
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => setShow(true)}
-              style={{
-                paddingLeft: '1rem',
-                color: 'white',
-                textDecoration: 'none',
-              }}
-            >
-              Register
-            </Nav.Link>
-            {/*
+	const [show, setShow] = useState(false);
+	const [selectedOption, setSelectedOption] = useState('School');
+	const [selectedOption2, setSelectedOption2] = useState('Country');
+	console.log(data2);
+	return (
+		<>
+			<Modal
+				show={show}
+				onHide={() => setShow(false)}
+				dialogClassName="modal-90w"
+				aria-labelledby="example-custom-modal-styling-title"
+			>
+				<Modal.Body>
+					<Form>
+						<Form.Control type="text" placeholder="First Name" required />
+						<br />
+						<Form.Control type="text" placeholder="Major" required />
+						<br />
+						<Form.Control type="text" placeholder="Last Name" />
+						<br />
+						<Form.Control type="text" placeholder="Email" />
+						<br />
+						<Form.Control type="text" placeholder="Phone Number" />
+						<br />
+						<Form.Select aria-label="Gender">
+							<option value="Unkown">Prefer Not Say</option>
+							<option value="Male">Male</option>
+							<option value="Female">Female</option>
+							<option value="Non-binary">Non-binary</option>
+						</Form.Select>
+						<br />
+						<Form.Select aria-label="Current level of study">
+							<option value="Other">Other</option>
+							<option value="Freshman">Freshman</option>
+							<option value="Sophmore">Sophmore</option>
+							<option value="Junior">Junior</option>
+							<option value="Senior">Senior</option>
+							<option value="Graduate">Graduate</option>
+						</Form.Select>
+						<br />
+						<Select
+							defaultValue={selectedOption}
+							onChange={setSelectedOption}
+							options={data}
+						/>
+						<br />
+						<Select
+							defaultValue={selectedOption2}
+							onChange={setSelectedOption2}
+							options={data2}
+						/>
+						<Form.Select aria-label="Loking for Opportunities">
+							<option value="Internship">Internship</option>
+							<option value="Full-time">Full-time</option>
+							<option value="Just here to hack">Just here to hack</option>
+						</Form.Select>
+						<br />
+					</Form>
+				</Modal.Body>
+			</Modal>
+			<Navbar
+				style={{ backgroundColor: '#8B0000', padding: '1rem' }}
+				variant="dark"
+				sticky="top"
+			>
+				<Container fluid>
+					<div
+						style={{
+							display: 'flex',
+							alignItems: 'center',
+							fontSize: '1.25rem',
+						}}
+					>
+						<Navbar.Brand href="#home">
+							<img
+								src={logo}
+								width="35"
+								height="32"
+								className="d-inline-block align-top"
+							/>
+							HackRPI X
+						</Navbar.Brand>
+						<Nav.Link
+							href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+							style={{
+								paddingLeft: '1rem',
+								color: 'white',
+								textDecoration: 'none',
+							}}
+						>
+							MLH Code of Conduct
+						</Nav.Link>
+						<Nav.Link
+							onClick={() => setShow(true)}
+							style={{
+								paddingLeft: '1rem',
+								color: 'white',
+								textDecoration: 'none',
+							}}
+						>
+							Register
+						</Nav.Link>
+						{/*
               href="https://my.mlh.io/oauth/authorize?client_id=c5u9kjQ8osE_5AgRrLnsXXvol1gR45AXrabR7upaox0&redirect_uri=https%3A%2F%2Fhackrpi.com%2F&response_type=token"
               <Nav.Link href="#about" style={{ paddingLeft: '1rem', color: 'white', textDecoration: 'none' }}>
                 About
@@ -109,11 +126,11 @@ export default function NavBar() {
                 Sponsors
               </Nav.Link>
                */}
-          </div>
-        </Container>
-      </Navbar>
-    </>
-  );
+					</div>
+				</Container>
+			</Navbar>
+		</>
+	);
 }
 
 // export default NavBar;
