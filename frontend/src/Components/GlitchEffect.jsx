83d81837475936aff213ba95edc0bc3d1a267501
marinocj <35199762+marinocj@users.noreply.github.com>
2023-07-20 14:49:42 -0400

    Added greyscale to glitch

--- frontend/src/Components/GlitchEffect.jsx
+++ frontend/src/Components/GlitchEffect.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const GlitchEffect = ({ children, style }) => {
   const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -6,6 +6,7 @@ const GlitchEffect = ({ children, style }) => {
   const [glitchedText, setGlitchedText] = useState('');
   const [originalText, setOriginalText] = useState('');
   const [isAnimating, setIsAnimating] = useState(false);
+  const h1Ref = useRef(null);
 
   useEffect(() => {
     const handleMouseOver = () => {
@@ -13,17 +14,19 @@ const GlitchEffect = ({ children, style }) => {
       setOriginalText(children);
       let iteration = -2;
       const interval = setInterval(() => {
-        setGlitchedText((prevText) =>
+        setGlitchedText(() =>
           originalText
             .split('')
             .map((letter, index) => {
               if (index < iteration || letter === ' ') {
                 return originalText[index];
               }
+              const grey = Math.floor(Math.random() * 256);
+              const color = `rgb(${grey}, ${grey}, ${grey})`;
               if (originalText[index] == 'X') {
-                return numbers[Math.floor(Math.random() * 9)];
+                return `<span style="color: ${color}; font-family: 'Mokoto'; font-size: 1em;">${numbers[Math.floor(Math.random() * 9)]}</span>`;
               }
-              return letters[Math.floor(Math.random() * 26)];
+              return `<span style="color: ${color}; font-family: 'Mokoto'; font-size: 1em;">${letters[Math.floor(Math.random() * 26)]}</span>`;
             })
             .join(''),
         );
@@ -33,8 +36,8 @@ const GlitchEffect = ({ children, style }) => {
           setIsAnimating(false);
         }
 
-        iteration += 1 / 3;
-      }, 30);
+        iteration += 1 / 15;
+      }, 10);
     };
 
     const handleMouseOut = () => {
@@ -43,7 +46,7 @@ const GlitchEffect = ({ children, style }) => {
       }
     };
 
-    const h1Element = document.querySelector('h1');
+    const h1Element = h1Ref.current;
     h1Element.addEventListener('mouseover', handleMouseOver);
     h1Element.addEventListener('mouseout', handleMouseOut);
 
@@ -53,7 +56,7 @@ const GlitchEffect = ({ children, style }) => {
     };
   }, [children, originalText, isAnimating]);
 
-  return <h1 style={style}>{glitchedText || children}</h1>;
+  return <h1 style={{...style, fontFamily: 'Mokoto'}} ref={h1Ref} dangerouslySetInnerHTML={{ __html: glitchedText || children }}></h1>;
 };
 
 export default GlitchEffect;
