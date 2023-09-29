771c860b6ab50078c2f9c0e4bfe74f00048bbe1f
Jordan <79342877+Jordan231111@users.noreply.github.com>
2023-09-29 19:56:43 -0400

    SchedulePage
    My Ideas for Schedule Page
    
    The files I created are pretty much the files that are required to make the Schedule Page.
    
    Changes to formatting can/will be worked on

---
+++ frontend/src/Pages/SchedulePage/SchedulePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Schedule from '../../Components/Schedule/Schedule.jsx';
+import './SchedulePage.css'; // Import your CSS file for SchedulePage styling
+
+const SchedulePage = () => {
+    return (
+        <div className="schedule-page-container">
+            <h1>Schedule</h1>
+            <Schedule />
+        </div>
+    );
+};
+
+export default SchedulePage;
