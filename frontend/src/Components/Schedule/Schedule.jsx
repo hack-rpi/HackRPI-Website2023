b9d91fd88ce06e33b46a531e320e19483c2d0be6
Jordan <79342877+Jordan231111@users.noreply.github.com>
2023-10-06 10:26:31 -0400

    Iteration 2
    Much more stable, no bugs noticed due to various tests

--- frontend/src/Components/Schedule/Schedule.jsx
+++ frontend/src/Components/Schedule/Schedule.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import './Schedule.css'; // FIX schedule import
+import '../../fonts.css';
 
-const tolerance = 15 * 1000; // 15 sec in milliseconds
+
+
+
+const tolerance = 10 * 1000; // 10 sec in milliseconds
 
 const ScheduleRow = React.memo(({ item, isCurrentEvent }) => {            //React.memo optimizes the rendering of the ScheduleRow component based on its props.
   const formatDate = (date) => date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
@@ -10,40 +13,40 @@ const ScheduleRow = React.memo(({ item, isCurrentEvent }) => {            //Reac
 
 
   return (
-    <tr style={{ backgroundColor: isCurrentEvent ? '#910307' : '#353535', padding: '0.5rem', borderBottom: '10px solid black' }}> 
-      <td style={{ color: isCurrentEvent ? 'white' : 'white', paddingRight: '1rem' }}>
-        {startTimeString} - {endTimeString}
-      </td>
-      <td style={{ color: isCurrentEvent ? 'white' : 'white', paddingRight: '1rem' }}>
-        {item.event}
-      </td>
-      <td style={{ color: isCurrentEvent ? 'white' : 'white' }}>{item.location}</td> 
-    </tr>                         //To avoid breaking the cold I will just make the conditional statement both equal white instead of trying to remove it. 
+    
+ <tr style={{ fontFamily: 'Poppins', backgroundColor: isCurrentEvent ? '#910307' : '#353535', padding: '0.5rem', borderBottom: '10px solid black' }}>
+    <td style={{ fontFamily: 'Poppins', color: 'white', paddingRight: '1rem', fontSize: '32px'}}>{item.event}</td>
+    <td style={{ fontFamily: 'Poppins', color: 'white', paddingRight: '1rem', fontSize: '32px' }}>{item.location}</td>  
+    <td style={{ fontFamily: 'Poppins', color: 'white', paddingRight: '1rem', fontSize: '32px' }}>{startTimeString} - {endTimeString}</td> 
+ </tr>
   );
-}); //Fix the style section formating of the code if have time is very unconcisse and hard to read.
+}); 
 //<div className="Textsize"> 
 const Schedule = () => {
-  const schedule = useMemo(() => [   //useMemo is used to optimize the initialization of the schedule array and prevents unnecessary rerender                
+  const schedule = useMemo(() => [   //useMemo is used to optimize the initialization of the schedule array and prevents unnecessary rerender    
+                
     {
-      startTime: new Date('2023-09-30T08:00:00-04:00'),
-      endTime: new Date('2023-09-30T20:30:00-04:00'),
+      startTime: new Date('2023-10-06T10:20:00-04:00'),
+      endTime: new Date('2023-10-06T10:21:00-04:00'),
       event: 'Event 1',
       location: 'Room A',
     },
     {
-      startTime: new Date('2023-09-30T15:00:00-04:00'),
-      endTime: new Date('2023-10-30T15:17:00-04:00'),
+      startTime: new Date('2023-10-06T10:21:00-04:00'),
+      endTime: new Date('2023-10-06T10:22:00-04:00'),
       event: 'Event 2',
       location: 'Room B',
     },
     {
-      startTime: new Date('2023-10-01T15:00:00-04:00'),
+      startTime: new Date('2023-10-09T15:10:17-04:00'),
       endTime: new Date('2023-10-02T12:16:00-04:00'),
       event: 'Event 3',
       location: 'Room B',
     },
-   // </div> //FIX THE CLASS NAME AND DIV 
-    // add more events here... MAKE SURE  IT IS -05:00 for daylight savings time on november 5th 
+
+     //* add more events here... MAKE SURE  IT IS -05:00 for daylight savings time on november 5th */}
+    //* separate nov and nov 5. */}
+
   ], []);
 
   const [currentEvent, setCurrentEvent] = useState(null);
@@ -78,19 +81,19 @@ const Schedule = () => {
           <p>Location: {currentEvent.location}</p>
         </div>
       ) : (
-        <p>No ongoing events.</p>          // If Current event is null, display no ongoing events, else display the current event.
+        <p>No ongoing events.</p>          // If Current event is true/happening, display the current event.else  display no ongoing events
       )}
 
       <h2></h2>                                
       <table className="schedule-table">
   <thead>
     <tr>
-      <th>Time</th>
       <th>Event</th>
       <th>Location</th>
+      <th>Time</th>
     </tr>
     <tr>
-      <td colSpan="3" style={{ borderBottom: '2px solid #bd0909' }}></td>    {/*injects a horizontal line , remove td in between <tr></tr> and the tr itself*/}
+      <td colSpan="3" style={{ borderBottom: '2px solid #bd0909' }}></td>    {/*injects a horizontal line ,if need to remove, , td in between <tr></tr> and the tr itself*/}
     </tr> 
   </thead>
   <tbody>
@@ -98,7 +101,7 @@ const Schedule = () => {
             const isCurrentEvent = currentEvent &&
               item.startTime.getTime() === currentEvent.startTime.getTime() &&
               item.event === currentEvent.event &&
-              item.location === currentEvent.location;  //multiple conditions are used to check if the current event is the same as the current item 
+              item.location === currentEvent.location;  //multiple conditions are used to check if the current event is the same as the current item and all components are updated togehter.
 
             return (
               <ScheduleRow key={index} item={item} isCurrentEvent={isCurrentEvent} /> // iterate through the schedule array
