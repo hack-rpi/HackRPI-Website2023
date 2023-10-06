f8218c330722233fe5bffa6b6dbc0fe06a0d5750
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2023-10-06 16:39:39 -0400

    Update MeetTheTeamPage.jsx
    Added database for team members, including their Name and Role for now. Added an outline for rendering their information as well.
    
    Co-Authored-By: ishipandey20 <99205059+ishipandey20@users.noreply.github.com>

--- frontend/src/Pages/MeetTheTeamPage/MeetTheTeamPage.jsx
+++ frontend/src/Pages/MeetTheTeamPage/MeetTheTeamPage.jsx
@@ -6,6 +6,44 @@ import Tab from 'react-bootstrap/Tab';
 import Button from 'react-bootstrap/Button';
 
 const MeetTheTeamPage = () => {
+    const teamMembers = [
+      {
+        name: 'Maryellen Marino',
+        role: 'President',
+        avatar: '', 
+      },
+      {
+        name: 'Raven Levitt',
+        role: 'Vice President',
+        avatar: '',
+      },
+
+      {
+        name: 'Xenia Khusid',
+        role: 'Director of Logistics',
+        avatar: '',
+      },
+
+      {
+        name: 'Adwait Naware',
+        role: 'Director of Finance',
+        avatar: '',
+      },
+
+      {
+        name: 'Sanay Tralshawala',
+        role: 'Director of Sponsorship',
+        avatar: '',
+      },
+
+      {
+        name: 'Miranda Zheng',
+        role: 'Director of Marketing',
+        avatar: '',
+      },
+
+    ];
+
     return (
       <div id="MeetTheTeam"  className="MeetTheTeamPage" style={{ textAlign: 'center', width: "80%", margin: 'auto'}}>
         <h1 className="title" style={{ fontFamily: 'Mokoto', fontSize: 46, height: 35 }}>
@@ -14,8 +52,17 @@ const MeetTheTeamPage = () => {
         <br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
         <br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
         <br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-        <div className="team-members">
-
+        <div className="team-members"> 
+        {/* Will map through team members data and render their information here */}
+          {/* 
+          {teamMembers.map((member, index) => (
+            <div key={index} className="team-member">
+              <img src={member.avatar} alt={member.name} />
+              <h2>{member.name}</h2>
+              <p>{member.role}</p>
+            </div>
+          ))}
+          */}
         </div>
       </div>
     );
