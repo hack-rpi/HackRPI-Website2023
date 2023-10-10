5ae0bb05a4a6e335a7cbdb91a65db6e5fcf5e1ba
milesthomas <ths22115@towerhill.org>
2023-10-10 15:01:26 -0400

    resized "404!" to fit mobile viewport :(

--- frontend/src/Pages/ErrorPage/ErrorPage.jsx
+++ frontend/src/Pages/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ function ErrorPage() {
   return (
     <div id="error-page" style={{height: '50vh', display: 'flex',
       flexDirection: 'column', justifyContent: 'center'}}>
-      <h1 style={{textAlign: "center", fontSize: '200px'}}>
+      <h1 style={{textAlign: "center", fontSize: '150px'}}>
         404!
       </h1>
       <h2 style={{textAlign: "center"}}>
