057cd1ec692095f4265bb580ae5f89b037a81d71
Maryellen Marino <maryellendmarino@gmail.com>
2023-10-11 14:19:56 -0400

    create home page to combine elements
    Co-Authored-By: Ali Lin <94757856+alin600@users.noreply.github.com>

---
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import LandingPage from './Pages/LandingPage/LandingPage.jsx';
+import AboutPage from './Pages/AboutPage/AboutPage.jsx';
+import SponsorPage from './Pages/SponsorPage/SponsorPage.jsx';
+
+const HomePage = () => {
+  return (
+    <div>
+      <LandingPage />
+      <AboutPage />
+      <SponsorPage />
+    </div>
+  );
+};
+
+export default HomePage;
