7d0d114ad9ef4092d0e359405741b1118e9d95c5
InahL <inah0331@gmail.com>
2023-10-13 09:47:54 -0400

    Update SponsorUsPage.jsx

--- frontend/src/Pages/SponsorUsPage/SponsorUsPage.jsx
+++ frontend/src/Pages/SponsorUsPage/SponsorUsPage.jsx
@@ -16,13 +16,25 @@ function SponsorUsPage() {
       Sponsor Us!
     </h2>
       <div style={{
-        display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center',
-        columnGap: '10%', padding: '2% 10%'
+        display: 'flex', 
+        flexDirection: 'row', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        columnGap: '10%', 
+        padding: '2% 10%'
       }}>
-        <iframe src="https://drive.google.com/file/d/17z1LSpUkCeY-AO3tYG5PfMv0U2FngzD_/preview"
-                width="35%" height="480" allow="autoplay"></iframe>
-        <iframe src="https://drive.google.com/file/d/17GguOFdq5aU5NuFXnHyLxxsXSzxryHQc/preview"
-                width="35%" height="480" allow="autoplay"></iframe>
+        <iframe 
+        src="https://drive.google.com/file/d/17z1LSpUkCeY-AO3tYG5PfMv0U2FngzD_/preview"
+        width="50%" 
+        height="550" 
+        allow="autoplay"
+        ></iframe>
+        <iframe 
+        src="https://drive.google.com/file/d/17GguOFdq5aU5NuFXnHyLxxsXSzxryHQc/preview"
+        width="50%" 
+        height="550" 
+        allow="autoplay"
+        ></iframe>
       </div>
     </div>
   );
