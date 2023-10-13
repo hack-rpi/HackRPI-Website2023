af2c98ccbec3c97989dc85113ca33097c8f53953
Maryellen Marino <maryellendmarino@gmail.com>
2023-10-13 11:14:27 -0400

    update styling for data/location
    Co-Authored-By: Ali Lin <94757856+alin600@users.noreply.github.com>

--- frontend/src/Pages/MiniEventPage/MiniEventPage.jsx
+++ frontend/src/Pages/MiniEventPage/MiniEventPage.jsx
@@ -4,22 +4,17 @@ import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 import Button from 'react-bootstrap/Button';
+import './MiniEventPage.css'; 
 
 const MiniEventPage = () => {
   return (
-    <div
-      id="mini-event"
-      className="MiniEventPage"
-      style={{ textAlign: 'center', width: '100%', padding: '0 10%' }}
-    >
-      <h1 style={{ fontFamily: 'Mokoto', fontSize: '3em', margin: '20px 0' }}>
-        HOW TO HACKRPI
-      </h1>
+    <div id="mini-event" className="MiniEventPage">
+      <h1 className="event-title">HOW TO HACKRPI</h1>
       <Tab.Container defaultActiveKey="one">
         <Row>
-          <Col xs={12}>
-            <p>Mini Event on October 21st 12pm-4pm</p>
-            <p>Location: LOW 4050</p>
+          <Col xs={12} className="event-details">
+            <p className="event-date">Mini Event on October 21st 12pm-4pm</p>
+            <p className="event-location">Location: LOW 4050</p>
           </Col>
         </Row>
       </Tab.Container>
@@ -27,11 +22,7 @@ const MiniEventPage = () => {
       <div className="info-section">
         <div className="register-button">
           <Nav.Link href="https://organize.mlh.io/participants/events/9892-hackrpi">
-            <Button
-              variant="outline"
-              className="button"
-              style={{ backgroundColor: '#910307', color: 'white' }}
-            >
+            <Button variant="outline" className="button register-btn">
               Register Now!
             </Button>
           </Nav.Link>
