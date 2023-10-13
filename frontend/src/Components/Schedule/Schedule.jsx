06eff81f925e3de1e51f05abfd9a9dac7e9c60be
Jordan <79342877+Jordan231111@users.noreply.github.com>
2023-10-13 15:06:53 -0400

    Final Iteration 2

--- frontend/src/Components/Schedule/Schedule.jsx
+++ frontend/src/Components/Schedule/Schedule.jsx
@@ -23,7 +23,7 @@ const ScheduleRow = React.memo(({ item, isCurrentEvent }) => {       //React.mem
 const Schedule = () => {
   const schedule = useMemo(() => [   //useMemo is NEEDED to optimize the initialization of the schedule array and prevents unnecessary rerender and overload.             
     {
-      startTime: new Date('2023-11-04T10:00:00-04:00'),
+      startTime: new Date('2023-11-04T10:00:00-04:00'), 
       endTime: new Date('2023-11-04T11:00:00-04:00'),
       event: 'Participant Check-In',
       location: 'DCC Lobby',
@@ -115,14 +115,14 @@ const Schedule = () => {
     {
       startTime: new Date('2023-11-04T17:30:00-04:00'),
       endTime: new Date('2023-11-04T18:30:00-04:00'),
-      event: 'FBI',
-      location: 'DCC 318',
+      event: 'Global Foundries',
+      location: 'DCC 327',
     },
     {
       startTime: new Date('2023-11-04T17:30:00-04:00'),
       endTime: new Date('2023-11-04T18:30:00-04:00'),
-      event: 'Global Foundries',
-      location: 'DCC 327',
+      event: 'Password Exploitation - FBI Albany ',
+      location: 'DCC 318',
     },
     {
       startTime: new Date('2023-11-04T18:30:00-04:00'),
@@ -132,7 +132,7 @@ const Schedule = () => {
     },
     {
       startTime: new Date('2023-11-04T18:30:00-04:00'),
-      endTime: new Date('2023-11-04T02:23:00-04:00'),
+      endTime: new Date('2023-11-04T23:00-04:00'),
       event: 'Smash Club',
       location: 'DCC 318',
     },
@@ -196,169 +196,169 @@ const Schedule = () => {
       event: 'Daylight Savings Party',
       location: 'DCC Lounge',
     },
-    //Account for daylight savings using GMT -5:00
-    {
-      startTime: new Date('2023-11-05T07:00:00-05:00'),
-      endTime: new Date('2023-11-05T09:00:00-05:00'),
-      event: 'Breakfast',
-      location: 'DCC Lounge',
-    },
-    {
-      startTime: new Date('2023-11-05T07:00:00-05:00'),
-      endTime: new Date('2023-11-05T09:00:00-05:00'),
-      event: 'Submission Deadline',
-      location: 'Online',
-    },
-    {
-      startTime: new Date('2023-11-05T11:30:00-05:00'),
-      endTime: new Date('2023-11-05T12:00:00-05:00'),
-      event: 'Hacking Ends @ 12',
-      location: '',
-    },
-    {
-      startTime: new Date('2023-11-05T12:00:00-05:00'),
-      endTime: new Date('2023-11-05T15:00:00-05:00'),
-      event: 'Showcase',
-      location: 'DCC 308',
-    },
-    {
-      startTime: new Date('2023-11-05T13:00:00-05:00'),
-      endTime: new Date('2023-11-05T14:00:00-05:00'),
-      event: 'Lunch',
-      location: 'DCC Lounge',
-    },
-    {
-      startTime: new Date('2023-11-05T15:00:00-05:00'),
-      endTime: new Date('2023-11-05T15:30:00-05:00'),
-      event: 'Closing Ceremony',
-      location: 'DCC Lounge',
-    },
+      //Account for daylight savings using GMT -5:00
+      {
+        startTime: new Date('2023-11-05T07:00:00-05:00'),
+        endTime: new Date('2023-11-05T09:00:00-05:00'),
+        event: 'Breakfast',
+        location: 'DCC Lounge',
+      },
+      {
+        startTime: new Date('2023-11-05T07:00:00-05:00'),
+        endTime: new Date('2023-11-05T09:00:00-05:00'),
+        event: 'Submission Deadline',
+        location: 'Online',
+      },
+      {
+        startTime: new Date('2023-11-05T11:30:00-05:00'),
+        endTime: new Date('2023-11-05T12:00:00-05:00'),
+        event: 'Hacking Ends @ 12',
+        location: '',
+      },
+      {
+        startTime: new Date('2023-11-05T12:00:00-05:00'),
+        endTime: new Date('2023-11-05T15:00:00-05:00'),
+        event: 'Showcase',
+        location: 'DCC 308',
+      },
+      {
+        startTime: new Date('2023-11-05T13:00:00-05:00'),
+        endTime: new Date('2023-11-05T14:00:00-05:00'),
+        event: 'Lunch',
+        location: 'DCC Lounge',
+      },
+      {
+        startTime: new Date('2023-11-05T15:00:00-05:00'),
+        endTime: new Date('2023-11-05T15:30:00-05:00'),
+        event: 'Closing Ceremony',
+        location: 'DCC Lounge',
+      },
 
 
-     //* add more events here... MAKE SURE  IT IS -05:00 for daylight savings time on november 5th 2am*/}
-    //* separate nov 4 and nov 5. */}  
-    // Critical issue: THE SCALING BREAKS ON very small MOBILE
-    
+      //* add more events here... MAKE SURE  IT IS -05:00 for daylight savings time on november 5th 2am*/}
+      //* separate nov 4 and nov 5. */}  
+      // Critical issue: THE SCALING BREAKS ON very small MOBILE
+      
 
-  ], []);
-  const constantEvents = useMemo(() => [
-    // Define your constant events here after November 5th
-    // Example:
-    {
-      startTime: new Date('2023-10-05T15:30:00-05:00'),
-      endTime: new Date('2023-11-05T16:30:00-05:00'),
-      event: 'Constant Event 1',
-      location: 'DCC 310',
-    },
-    {
-      startTime: new Date('2023-11-05T17:00:00-05:00'),
-      endTime: new Date('2023-11-05T18:00:00-05:00'),
-      event: 'Constant Event 2',
-      location: 'DCC 312',
-    },
-    // },
-  ], []);
+    ], []);
+    const constantEvents = useMemo(() => [
+      // Define your constant events here after November 5th
+      // Example:
+      {
+        startTime: new Date('2023-10-05T15:30:00-05:00'),
+        endTime: new Date('2023-11-05T16:30:00-05:00'),
+        event: 'Constant Event 1',
+        location: 'DCC 310',
+      },
+      {
+        startTime: new Date('2023-11-05T17:00:00-05:00'),
+        endTime: new Date('2023-11-05T18:00:00-05:00'),
+        event: 'Constant Event 2',
+        location: 'DCC 312',
+      },
+      // },
+    ], []);
 
-  const [currentEvent, setCurrentEvent] = useState(null);
+    const [currentEvent, setCurrentEvent] = useState(null);
 
-  useEffect(() => {
-    const updateCurrentEvent = () => {
-      const currentTime = new Date().getTime();
-      const runningEvent = schedule.find((event) => {
-        const startTime = event.startTime.getTime();
-        const endTime = event.endTime.getTime();
-        return currentTime >= startTime && currentTime <= endTime + tolerance;
-      });
+    useEffect(() => {
+      const updateCurrentEvent = () => {
+        const currentTime = new Date().getTime();
+        const runningEvent = schedule.find((event) => {
+          const startTime = event.startTime.getTime();
+          const endTime = event.endTime.getTime();
+          return currentTime >= startTime && currentTime <= endTime + tolerance;
+        });
 
-      setCurrentEvent(runningEvent || null);
-    };
+        setCurrentEvent(runningEvent || null);
+      };
 
-    updateCurrentEvent();
+      updateCurrentEvent();
 
-    const intervalId = setInterval(updateCurrentEvent, 60000);  //updates every minute, setting it shorter is not ideal
+      const intervalId = setInterval(updateCurrentEvent, 60000);  //updates every minute, setting it shorter is not ideal
 
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [schedule, tolerance]);
-    
-  let currentDate = null; // To keep track of the current date
-  // Critical Issue Events may be missing despite being on the schedule above.
-  return (
-    <div>
-     
-      <table className="schedule-table">
-        <thead>
-          <tr>
-            <th style={{ fontFamily: 'Poppins', color: 'white', paddingRight: '2rem', fontSize: '32px', textAlign: 'center', verticalAlign: 'middle' }}>Event</th>
-            <th style={{ fontFamily: 'Poppins', color: 'white', paddingRight: '2rem', fontSize: '32px', textAlign: 'center', verticalAlign: 'middle' }}>Location</th>
-            <th style={{ fontFamily: 'Poppins', color: 'white', fontSize: '32px', textAlign: 'center', verticalAlign: 'middle' }}>Time</th>
-          </tr>
-        </thead>
-        <tbody>
-        {
-  /* Render Events */
-  schedule.map((item, index) => {
-    // Use a loop to Check if the current event's date is different from the previous event's date
-    if (item.startTime.getDate() !== currentDate) {
-      // Render a row with the date as the heading
-      currentDate = item.startTime.getDate();
-      return (
-        <tr key={`date-heading-${currentDate}`}>
-          <td colSpan="3" style={{ fontFamily: 'Poppins', color: 'white', borderBottom: '1.5px solid #bd0909', textAlign: 'center', fontSize: '32px' }}>
-            {currentDate === 4 ? 'November 4th' : 'November 5th'}
-          </td>
-        </tr>
-      );
-    }
-    // Determine if the current event should be highlighted
-    const isCurrentEvent =
-      currentEvent &&
-      currentEvent.startTime >= item.startTime &&
-      currentEvent.endTime <= item.endTime;
-
-    // Render individual event row using a fragment
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, [schedule, tolerance]);
+      
+    let currentDate = null; // To keep track of the current date
+    // Critical Issue Events may be missing despite being on the schedule above.
     return (
-      <React.Fragment key={index}>
-        <ScheduleRow
-          item={item}
-          isCurrentEvent={isCurrentEvent}
-        />
-      </React.Fragment>
-    );
-  })
-}
-
-          {/* Constant Events */}
-                  {/* Blank row for spacing after November 5th events */}
-        <tr>
-          <td colSpan="3" style={{ height: '50px' }}></td>
-        </tr>
-          <tr>
+      <div>
+      
+        <table className="schedule-table">
+          <thead>
+            <tr>
+              <th style={{ fontFamily: 'Poppins', color: 'white', paddingRight: '2rem', fontSize: '32px', textAlign: 'center', verticalAlign: 'middle' }}>Event</th>
+              <th style={{ fontFamily: 'Poppins', color: 'white', paddingRight: '2rem', fontSize: '32px', textAlign: 'center', verticalAlign: 'middle' }}>Location</th>
+              <th style={{ fontFamily: 'Poppins', color: 'white', fontSize: '32px', textAlign: 'center', verticalAlign: 'middle' }}>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+    /* Render Events */
+    schedule.map((item, index) => {
+      // Use a loop to Check if the current event's date is different from the previous event's date
+      if (item.startTime.getDate() !== currentDate) {
+        // Render a row with the date as the heading
+        currentDate = item.startTime.getDate();
+        return (
+          <tr key={`date-heading-${currentDate}`}>
             <td colSpan="3" style={{ fontFamily: 'Poppins', color: 'white', borderBottom: '1.5px solid #bd0909', textAlign: 'center', fontSize: '32px' }}>
-              Constant Events
+              {currentDate === 4 ? 'November 4th' : 'November 5th'}
             </td>
           </tr>
-          {/* Render Constant Events */}
-          {constantEvents.map((item, index) => {
-            const currentTime = new Date().getTime();
-            const startTime = item.startTime.getTime();
-            const endTime = item.endTime.getTime();
-            const isCurrentEvent = currentTime >= startTime && currentTime <= endTime + tolerance;
+        );
+      }
+      // Determine if the current event should be highlighted
+      const isCurrentEvent =
+        currentEvent &&
+        currentEvent.startTime >= item.startTime &&
+        currentEvent.endTime <= item.endTime;
 
-            return (
-              <React.Fragment key={index}>
-                {/* Render individual constant event row */}
-                <ScheduleRow item={item} isCurrentEvent={isCurrentEvent} />
-              </React.Fragment>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+      // Render individual event row using a fragment
+      return (
+        <React.Fragment key={index}>
+          <ScheduleRow
+            item={item}
+            isCurrentEvent={isCurrentEvent}
+          />
+        </React.Fragment>
+      );
+    })
+  }
+
+            {/* Constant Events */}
+                    {/* Blank row for spacing after November 5th events */}
+          <tr>
+            <td colSpan="3" style={{ height: '50px' }}></td>
+          </tr>
+            <tr>
+              <td colSpan="3" style={{ fontFamily: 'Poppins', color: 'white', borderBottom: '1.5px solid #bd0909', textAlign: 'center', fontSize: '32px' }}>
+                Constant Events
+              </td>
+            </tr>
+            {/* Render Constant Events */}
+            {constantEvents.map((item, index) => {
+              const currentTime = new Date().getTime();
+              const startTime = item.startTime.getTime();
+              const endTime = item.endTime.getTime();
+              const isCurrentEvent = currentTime >= startTime && currentTime <= endTime + tolerance;
+
+              return (
+                <React.Fragment key={index}>
+                  {/* Render individual constant event row */}
+                  <ScheduleRow item={item} isCurrentEvent={isCurrentEvent} />
+                </React.Fragment>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
 
 
 
-export default Schedule;
+  export default Schedule;
