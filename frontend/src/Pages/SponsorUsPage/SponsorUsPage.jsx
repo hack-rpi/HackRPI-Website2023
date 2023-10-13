4162b60cbdc4f6c5db08dcc9332efb31b4b45b99
ths22115 <xselymx@gmail.com>
2023-10-12 21:44:47 -0400

    implemented pre-made embedded item links found from Google Drive pdf links and added some preliminary styling -> def still needs work

--- frontend/src/Pages/SponsorUsPage/SponsorUsPage.jsx
+++ frontend/src/Pages/SponsorUsPage/SponsorUsPage.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import Iframe from 'react-iframe';
-import SponsershipPDF from './../../../assets/pdfs/HackRPI Sponsorship Booklet 2023.pdf'
 
 function SponsorUsPage() {
   return (
-    <div className="pdf-and-logo">
-      <div className="pdf">
-        <Iframe
-          src='https://drive.google.com/drive/u/0/folders/1bvx5a6eXrHhq8l9B3P5Dv-1JEcp-kUg-'
-          className="pdf-iframe"
-        ></Iframe>
-      </div>
-      <div className="pdf">
-        <Iframe
-          src="https://drive.google.com/drive/u/0/folders/1bvx5a6eXrHhq8l9B3P5Dv-1JEcp-kUg-"
-          className="pdf-iframe"
-        ></Iframe>
+    <div>
+      <div style={{
+        display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center',
+        columnGap: '10%', padding: '2% 10%'
+      }}>
+        <iframe src="https://drive.google.com/file/d/17z1LSpUkCeY-AO3tYG5PfMv0U2FngzD_/preview"
+                width="35%" height="480" allow="autoplay"></iframe>
+        <iframe src="https://drive.google.com/file/d/17GguOFdq5aU5NuFXnHyLxxsXSzxryHQc/preview"
+                width="35%" height="480" allow="autoplay"></iframe>
       </div>
     </div>
   );
