9d9950b85113200dfb6e1ffdac20e147a1bfe11c
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2023-10-13 17:11:13 -0400

    Progress bar added to the timer

--- frontend/src/Components/Timer/Timer.jsx
+++ frontend/src/Components/Timer/Timer.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 
-const FULL_DASH_ARRAY = 283;
-// needs serious revious
+const FULL_DASH_ARRAY = 407;
 class Timer extends React.Component {
 
   constructor() {
@@ -30,13 +29,37 @@ class Timer extends React.Component {
     }, 1000);
   }
 
-  setCircleDasharray() {
+  setCircleDasharray(remainingTime) {
+    const totalMilliseconds = 1000 * 60 * 60 * 24;
+    const days = Math.floor(remainingTime / totalMilliseconds);
+    const hours = Math.floor((remainingTime % totalMilliseconds) / (1000 * 60 * 60));
+    const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-    const secondsArray = `${(
-      (seconds/60) * FULL_DASH_ARRAY).toFixed(0)} 283`;
+    const daysArray = `${(
+      (days/30) * FULL_DASH_ARRAY).toFixed(0)} 407`;
+    var circleElement = document.getElementById('daysTimer');
+    if (circleElement) {
+      circleElement.setAttribute('stroke-dasharray', daysArray);
+    }
 
-    const circleElement = document.getElementById('base-timer-path-remaining');
+    const hoursArray = `${(
+      (hours/24) * FULL_DASH_ARRAY).toFixed(0)} 407`;
+    var circleElement = document.getElementById('hoursTimer');
+    if (circleElement) {
+      circleElement.setAttribute('stroke-dasharray', hoursArray);
+    }
+
+    const minutesArray = `${(
+      (minutes/60) * FULL_DASH_ARRAY).toFixed(0)} 407`;
+    var circleElement = document.getElementById('minutesTimer');
+    if (circleElement) {
+      circleElement.setAttribute('stroke-dasharray', minutesArray);
+    }
+
+    const secondsArray = `${(
+      (seconds/60) * FULL_DASH_ARRAY).toFixed(0)} 407`;
+    var circleElement = document.getElementById('secondsTimer');
     if (circleElement) {
       circleElement.setAttribute('stroke-dasharray', secondsArray);
     }
@@ -50,31 +73,6 @@ class Timer extends React.Component {
     clearInterval(this.timer);
   }
 
-  // updateCountdownBars(remainingTime) {
-  //   const daysCircle = document.getElementById('days-circle');
-  //   const hoursCircle = document.getElementById('hours-circle');
-  //   const minutesCircle = document.getElementById('minutes-circle');
-  //   const secondsCircle = document.getElementById('seconds-circle');
-
-  //   const totalMilliseconds = 1000 * 60 * 60 * 24;
-  //   const days = Math.floor(remainingTime / totalMilliseconds);
-  //   const hours = Math.floor((remainingTime % totalMilliseconds) / (1000 * 60 * 60));
-  //   const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-  //   const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
-
-  //   // Update the styles of the circular countdown bars
-  //   const daysRotation = (360 * (days % 30)) / 30;
-  //   const hoursRotation = (360 * (hours % 24)) / 24;
-  //   const minutesRotation = (360 * (minutes % 60)) / 60;
-  //   const secondsRotation = (360 * (seconds % 60)) / 60;
-
-  //   daysCircle.style.transform = `rotate(${daysRotation}deg)`;
-  //   hoursCircle.style.transform = `rotate(${hoursRotation}deg)`;
-  //   minutesCircle.style.transform = `rotate(${minutesRotation}deg)`;
-  //   secondsCircle.style.transform = `rotate(${secondsRotation}deg)`;
-  // }
-
-
   // Divides time left by the defined time limit.
   calculateTimeFraction() {
     return timeLeft / TIME_LIMIT;
@@ -85,79 +83,65 @@ class Timer extends React.Component {
 
   render() {
 
-
     return (
       <Container className='timer' >
-
-        <div className='contain'>
-          <div className='outerCircle'>
-            <div className='innerCircle'>
-
-              <div id="number"> 
-               {Math.floor((this.state.time % (1000 * 60)) / 1000)}
-              </div>
-
-            </div>
-          </div>
-
-          <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width="10vw" height="10vw" >
-            <circle cx="72" cy="72" r="60.75" stroke-linecap="round"/>
-            <circle cx="72" cy="72" r="60.75" id='base-timer-path-remaining' strokeDasharray="90 480"/>
-          </svg>
-        </div>
-
-        {/* <Col className='colStyle'>
-
+        <Col className="colStyle">
           <div className='groupStyle'>
-
-                <div className='outerCicle'>
-                  <div className='innterCircle'>
-                    nn
-                  </div>
+              <div className='contain'>
+                <div id="number"> 
+                  {Math.floor(this.state.time / (1000 * 60 * 60 * 24))}
                 </div>
 
-            </div>
-
-
-
-          <div className='groupStyle'>
-            <div className='circleStyle' >
-
-              <div className='outerCicle'>
-                <div className='innterCircle'>
-
-                </div>
+                <svg id='image' xmlns='http://www.w3.org/2000/svg' version='1.1' width="144px" height="144px" >
+                  <circle cx="72" cy="72" r="65" stroke-linecap="round"/>
+                  <circle cx="72" cy="72" r="65" class='base-timer-path-remaining' id='daysTimer' strokeDasharray="90 380"/>
+                </svg>
               </div>
-
-              {Math.floor(this.state.time / (1000 * 60 * 60 * 24))}
-            </div>
-            <div className='labelStyle'>DAYS</div>
+              <div className='labelStyle'>DAYS</div>
           </div>
 
           <div className='groupStyle'>
-            <div className='circleStyle' >
-              {Math.floor(
-                (this.state.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-              )}
-            </div>
-            <div className='labelStyle'>HOURS</div>
+              <div className='contain'>
+                <div id="number"> 
+                {Math.floor((this.state.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),)}
+                </div>
+
+                <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width="144px" height="144px" >
+                  <circle cx="72" cy="72" r="65" stroke-linecap="round"/>
+                  <circle cx="72" cy="72" r="65" class='base-timer-path-remaining' id='hoursTimer' strokeDasharray="90 380"/>
+                </svg>
+              </div>
+              <div className='labelStyle'>HOURS</div>
           </div>
 
           <div className='groupStyle'>
-            <div className='circleStyle' >
-              {Math.floor((this.state.time % (1000 * 60 * 60)) / (1000 * 60))}
-            </div>
-            <div className='labelStyle'>MINUTES</div>
+              <div className='contain'>
+                <div id="number"> 
+                  {Math.floor((this.state.time % (1000 * 60 * 60)) / (1000 * 60))}
+                </div>
+
+                <svg id='image' xmlns='http://www.w3.org/2000/svg' version='1.1' width="144px" height="144px" >
+                  <circle cx="72" cy="72" r="65" stroke-linecap="round"/>
+                  <circle cx="72" cy="72" r="65" class='base-timer-path-remaining' id='minutesTimer' strokeDasharray="90 380"/>
+                </svg>
+              </div>
+              <div className='labelStyle'>MINUTES</div>
           </div>
 
           <div className='groupStyle'>
-            <div className='circleStyle' >
-              {Math.floor((this.state.time % (1000 * 60)) / 1000)}
-            </div>
-            <div className='labelStyle'>SECONDS</div>
-          </div>
+              <div className='contain'>
+                <div id="number"> 
+                  {Math.floor((this.state.time % (1000 * 60)) / 1000)}
+                </div>
 
-        </Col> */}
+                <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width="144px" height="144px" >
+                  <circle cx="72" cy="72" r="65" stroke-linecap="round"/>
+                  <circle cx="72" cy="72" r="65" class='base-timer-path-remaining' id='secondsTimer' strokeDasharray="90 380"/>
+                </svg>
+              </div>
+              <div className='labelStyle'>SECONDS</div>
+          </div>
+        </Col>
 
       </Container>
     );
