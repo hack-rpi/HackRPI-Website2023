e9da26cde89f83abe4ad41ef70961614435fe051
Maryellen Marino <maryellendmarino@gmail.com>
2023-10-13 11:38:02 -0400

    styling & content updates, add gears
    Co-Authored-By: Ali Lin <94757856+alin600@users.noreply.github.com>

--- frontend/src/Pages/MiniEventPage/MiniEventPage.jsx
+++ frontend/src/Pages/MiniEventPage/MiniEventPage.jsx
@@ -4,29 +4,35 @@ import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 import Button from 'react-bootstrap/Button';
-import './MiniEventPage.css'; 
+import './MiniEventPage.css';
 
 const MiniEventPage = () => {
   return (
     <div id="mini-event" className="MiniEventPage">
       <h1 className="event-title">HOW TO HACKRPI</h1>
-      <Tab.Container defaultActiveKey="one">
-        <Row>
-          <Col xs={12} className="event-details">
-            <p className="event-date">Mini Event on October 21st 12pm-4pm</p>
-            <p className="event-location">Location: LOW 4050</p>
-          </Col>
-        </Row>
-      </Tab.Container>
+      <Row>
+        <Col xs={12} className="event-details">
+          <p className="event-date">Date: October 21st 12pm-4pm</p>
+          <p className="event-location">Location: LOW 4050</p>
+        </Col>
+      </Row>
 
       <div className="info-section">
-        <div className="register-button">
-          <Nav.Link href="https://organize.mlh.io/participants/events/9892-hackrpi">
+        <div className="buttons-container">
+          <Nav.Link href="https://forms.gle/oW3W5Yx6jbNyKfbeA">
             <Button variant="outline" className="button register-btn">
-              Register Now!
+              Register To Attend
+            </Button>
+          </Nav.Link>
+          <Nav.Link href="https://forms.gle/BLoxEemEaKKbzXpt7">
+            <Button variant="outline" className="button new-btn">
+              Sign Up to Mentor
             </Button>
           </Nav.Link>
         </div>
+        <p className="additional-info">
+          New to hackathons, web development, coding?
+        </p>
       </div>
     </div>
   );
