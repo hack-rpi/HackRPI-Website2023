6a90216fe527a78ebe3b092d5812edcf22d63cc8
Maryellen Marino <maryellendmarino@gmail.com>
2023-10-13 20:20:23 -0400

    fixed routing
    Co-Authored-By: marinocj <35199762+marinocj@users.noreply.github.com>

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,131 +8,135 @@ import FAQPage from './Pages/FAQPage/FAQPage.jsx';
 import MiniEventPage from './Pages/MiniEventPage/MiniEventPage.jsx';
 import Footer from './Components/Footer/Footer.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SponsorPage from './Pages/SponsorPage/SponsorPage.jsx';
 import SponsorUsPage from './Pages/SponsorUsPage/SponsorUsPage.jsx';
 import './style.css';
 import SchedulePage from './Pages/SchedulePage/SchedulePage.jsx';
-
 import { Outlet } from 'react-router-dom';
 import ErrorPage from './Pages/ErrorPage/ErrorPage.jsx';
-import { createHashRouter, RouterProvider, } from "react-router-dom";
-
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 const router = createHashRouter([
-	{
-		path: "/",
-        element: (
-            <div>
-                <NavBar />
-                <Layout />
-            </div>
-        ),
-	  children: [
-		{
-		  index: true,
-		  element:<HomePage />,
-		},
-		{
-			path: "home",
-			element: <HomePage />,
-		},
-		{
-		  path: "faq",
-		  element: <FAQPage />,
-		},
-        {
-            path: "mini-event",
-            element: <MiniEventPage />,
-        },
-        {
-            path: 'sponsor-us',
-            element: <SponsorUsPage />,
-        },
-		{
-		  path: "*",
-		  element: <ErrorPage/>,
-		},
-	  ],
-	},
-  ]);
-  
-
-const mobileRouter = createHashRouter([
-    {
-        path: "/",
-        element: (
-            <div>
-                <MobileNavBar />
-                <Layout />
-            </div>
-        ),
-        children: [
-            {
-                index: true,
-                element: <HomePageMobile />,
-            },
-			{
-                path: 'home',
-                element: <HomePageMobile />,
-            },
-            {
-                path: 'faq',
-                element: <FAQPage />,
-            },
-            {
-                path: 'mini-event',
-                element: <MiniEventPage />,
-            },
-            {
-                path: 'sponsor-us',
-                element: <SponsorUsPage />,
-            },
-            {
-                path: '*',
-                element: <ErrorPage />,
-            },
-        ]
-    },
+  {
+    path: '/',
+    element: (
+      <div>
+        <NavBar />
+        <Layout />
+      </div>
+    ),
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'home',
+        element: <HomePage />,
+      },
+      {
+        path: 'faq',
+        element: <FAQPage />,
+      },
+      {
+        path: 'schedule',
+        element: <SchedulePage />,
+      },
+      {
+        path: 'mini-event',
+        element: <MiniEventPage />,
+      },
+      {
+        path: 'sponsor-us',
+        element: <SponsorUsPage />,
+      },
+      {
+        path: '*',
+        element: <ErrorPage />,
+      },
+    ],
+  },
 ]);
 
+const mobileRouter = createHashRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <MobileNavBar />
+        <Layout />
+      </div>
+    ),
+    children: [
+      {
+        index: true,
+        element: <HomePageMobile />,
+      },
+      {
+        path: 'home',
+        element: <HomePageMobile />,
+      },
+      {
+        path: 'faq',
+        element: <FAQPage />,
+      },
+      {
+        path: 'schedule',
+        element: <SchedulePage />,
+      },
+      {
+        path: 'mini-event',
+        element: <MiniEventPage />,
+      },
+      {
+        path: 'sponsor-us',
+        element: <SponsorUsPage />,
+      },
+      {
+        path: '*',
+        element: <ErrorPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-    const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 900);
-        };
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 900);
+    };
 
-        handleResize(); // Check initial screen size
+    handleResize(); // Check initial screen size
 
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-    return (
-        <div>
-            {isMobile ? <RouterProvider router={mobileRouter}>
-                            <Layout />
-                        </RouterProvider>
-                : <RouterProvider router={router}>
-                    <Layout />
-                  </RouterProvider>
-            }
-        </div>
-    );
+  return (
+    <div>
+      {isMobile ? (
+        <RouterProvider router={mobileRouter}>
+          <Layout />
+        </RouterProvider>
+      ) : (
+        <RouterProvider router={router}>
+          <Layout />
+        </RouterProvider>
+      )}
+    </div>
+  );
 }
 
 function Layout() {
-    return (
-        <div>
-            <Outlet /> {/* Outlet = Current Route */}
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Outlet /> {/* Outlet = Current Route */}
+      <Footer />
+    </div>
+  );
 }
-
 
 export default App;
