8f6e22cebe4410f3d5cda2a1a56bbbb0619ac351
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2023-10-20 17:39:44 -0400

    Created Database for Workshop Page
    Added a database for the Workshop Page which includes all the current workshops as objects, as well as its title, time, location, speaker, and workshop picture as properties. Locations and pics are TBD.
    
    Co-Authored-By: ishipandey20 <99205059+ishipandey20@users.noreply.github.com>

--- frontend/src/Pages/WorkshopPage/WorkshopPage.jsx
+++ frontend/src/Pages/WorkshopPage/WorkshopPage.jsx
@@ -5,6 +5,57 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 
 const WorkshopPage = () => {
+
+    const workshops = [
+        {
+            title: ' Intro to MERN Stack Web Dev Workshop',
+            time: '1:30-2:30',
+            location: '',
+            speaker: 'Yash Kaul',
+            pic: '',
+        },
+        
+        {
+            title: 'Password Exploitation - FBI Albany',
+            time: '5:30-6:30',
+            location: '',
+            speaker: 'Alex Vargas and Ryan Gallagher',
+            pic: '',
+        },
+
+        {
+            title: 'Hacking the machine for fun (and sometimes profit)',
+            time: '4:30-5:30',
+            location: '',
+            speaker: ' Dr. Brian Callahan (w/ Ayah Tharwat )',
+            pic: '',
+        },
+
+        {
+            title: 'Patient Safety 101',
+            time: '12-12:30',
+            location: '',
+            speaker: 'TBD',
+            pic: '',
+        },
+
+        {
+            title: 'Design Thinking for Technical Innovation',
+            time: '4:30-5:30',
+            location: '',
+            speaker: 'Kylinn Askew',
+            pic: '',
+        },
+
+        {
+            title: 'AI made easy with Hugging Face',
+            time: '6:30-7:30',
+            location: '',
+            speaker: 'Raven Levitt',
+            pic: '',
+        },
+        // Add more workshop objects as needed
+    ];
   
   return (
     <div id="workshops" className="WorkshopPage" style={{ textAlign: 'center', width: '80%', margin: 'auto' }}>
