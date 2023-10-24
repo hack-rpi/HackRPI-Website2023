78ac304abb23f6a29d6f6f867205891d8895e436
Kai <cjiakai0824@gmail.com>
2023-10-24 12:14:56 -0400

    Tab Adjustment
    The previous code's tab container was broken. Modified it so that the drop-down menu pushes the other tabs down based on the height.

--- frontend/src/Pages/PrizesPage/tab.jsx
+++ frontend/src/Pages/PrizesPage/tab.jsx
@@ -1,6 +1,5 @@
-//import styled, { keyframes } from 'styled-components';
 import styled, { keyframes, css } from 'styled-components';
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const spin = keyframes`
   from {
@@ -13,6 +12,7 @@ const spin = keyframes`
 
 const TabContainer = styled.div`
   display: flex;
+  flex-direction: row; // changed from column to row
   align-items: center;
   cursor: pointer;
   white-space: nowrap;
@@ -22,7 +22,7 @@ const TabContainer = styled.div`
 
 const Gear = styled.div`
   font-size: 32px;
-  margin-right: 10px;
+  // Removed margin-bottom as it's not needed anymore
   animation: ${props => (props.spinning ? css`${spin} 2s linear infinite` : 'none')};
   user-select: none; 
 `;
@@ -30,17 +30,12 @@ const Gear = styled.div`
 const Dropdown = styled.div`
   background-color: #353535;  
   border: 1px solid #ccc;
-  position: absolute;
   z-index: 1;
-  top: 50px;
   width: 250px; 
   max-height: ${props => (props.open ? '500px' : '0')}; 
   overflow: hidden;
   transition: max-height 0.5s ease-in-out;
   border: none;
-
-  height: ${props => (props.open ? `${props.height}px` : '0')};
-  transition: height 0.5s ease-in-out;
 `;
 
 const Prize = styled.div`
@@ -51,16 +46,6 @@ const Prize = styled.div`
 const Tab = ({ title, prizes }) => {
   const [open, setOpen] = useState(false);
   const [spinning, setSpinning] = useState(false);
-  const [dropdownHeight, setDropdownHeight] = useState(0);
-  const dropdownRef = useRef(null);
-
-  useEffect(() => {
-    if (open && dropdownRef.current) {
-      setDropdownHeight(dropdownRef.current.getBoundingClientRect().height);
-    } else {
-      setDropdownHeight(0);
-    }
-  }, [open]);
 
   const handleGearClick = () => {
     setSpinning(true);
@@ -69,16 +54,12 @@ const Tab = ({ title, prizes }) => {
   };
 
   return (
-    <div style={{ 
-      position: 'relative', 
-      marginRight: '40px',
-      marginBottom: `${dropdownHeight}px` 
-    }}>
+    <div style={{ marginRight: '40px' }}>
       <TabContainer onClick={handleGearClick}>
         <Gear spinning={spinning}>⚙️</Gear>
         {title}
       </TabContainer>
-      <Dropdown ref={dropdownRef} open={open}>
+      <Dropdown open={open}>
         {prizes.map((prize, index) => (
           <Prize key={index}>{prize.title}: {prize.amount}</Prize>
         ))}
