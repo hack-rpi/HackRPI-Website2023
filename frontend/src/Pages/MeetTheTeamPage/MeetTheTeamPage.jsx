578174ec448c6105021fdc7c920ea014c54cd5c2
ishipandey20 <pandei@rpi.edu>
2023-10-24 16:38:33 -0400

    Changed Directors Pictures
    Swapped avatars for real images of directors and edited the image to be displayed as a circle

--- frontend/src/Pages/MeetTheTeamPage/MeetTheTeamPage.jsx
+++ frontend/src/Pages/MeetTheTeamPage/MeetTheTeamPage.jsx
@@ -5,13 +5,13 @@
   import Tab from 'react-bootstrap/Tab';
   import Button from 'react-bootstrap/Button';
   import './style.css';
-  import maryellenImage from './../../../assets/TeamAvatars/Maryellen.png';
-  import ravenImage from './../../../assets/TeamAvatars/Raven.png';
-  import xeniaImage from './../../../assets/TeamAvatars/Xenia.png';
-  import adwaitImage from './../../../assets/TeamAvatars/Adwait.png';
-  import sanayImage from './../../../assets/TeamAvatars/Sanay.png';
-  import mirandaImage from './../../../assets/TeamAvatars/Miranda.png';
-  import johnImage from './../../../assets/TeamAvatars/John.png';
+  import maryellenImage from './../../../assets/TeamPics/mel image.jpeg';
+  import ravenImage from './../../../assets/TeamPics/raven image.jpg';
+  import xeniaImage from './../../../assets/TeamPics/xenia image.jpg';
+  import adwaitImage from './../../../assets/TeamPics/adwait image.jpg';
+  import sanayImage from './../../../assets/TeamPics/sanay image.png';
+  import mirandaImage from './../../../assets/TeamPics/miranda image.jpg';
+  import johnImage from './../../../assets/TeamPics/john image.jpg';
 
   const MeetTheTeamPage = () => {
     const teamMembersFirstRow = [
@@ -61,14 +61,17 @@
           Meet Our Team!
         </h1>
         <br></br>
-  
         <div className="team-members">
+          <h1 style={{ fontFamily: 'Mokoto', fontSize: 30, marginBottom: '20px' }}>
+            Meet The Directors
+          </h1>
+          <br></br>
           <Row className="mb-4 justify-content-center">
             {/* First Row */}
             {teamMembersFirstRow.map((member, index) => (
               <Col key={index} md={3}>
-                <div className="" style={{ marginBottom: '20px' }}>
-                  <img src={member.avatar} alt={member.name} style={{ width: '70%' }} />
+                <div className="" style={{ marginBottom: '20px'}}>
+                  <img src={member.avatar} alt={member.name} style={{ width: '200px', height: '200px', objectFit: 'cover', borderRadius: '50%'}} />
                   <div className="container">
                     <h2 className="name">{member.name}</h2>
                     <p className="position">{member.role}</p>
@@ -86,7 +89,7 @@
             {teamMembersSecondRow.map((member, index) => (
               <Col key={index} md={3}>
                 <div className="" style={{ marginBottom: '20px' }}>
-                  <img src={member.avatar} alt={member.name} style={{ width: '70%' }} />
+                  <img src={member.avatar} alt={member.name} style={{ width: '200px', height: '200px', objectFit: 'cover',  borderRadius: '50%' }} />
                   <div className="container">
                     <h2 className="name">{member.name}</h2>
                     <p className="position">{member.role}</p>
@@ -99,6 +102,14 @@
             ))}
           </Row>
           <br></br>
+          <h1 style={{ fontFamily: 'Mokoto', fontSize: 30, marginBottom: '20px' }}>
+          Meet the Junior Directors
+          </h1>
+          <br></br>
+
+
+
+
           {/* Tab content and buttons */}
           <p style={{fontSize: '2em', fontWeight: 'bold'}}>
             Want to Help?
