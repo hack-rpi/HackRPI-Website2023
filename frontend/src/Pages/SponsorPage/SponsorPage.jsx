ac5fcdd7e6081b6948de3db35ea396f4c7e2cd97
alin600 <alilin.al6@gmail.com>
2023-10-26 12:18:34 -0400

    center companies

--- frontend/src/Pages/SponsorPage/SponsorPage.jsx
+++ frontend/src/Pages/SponsorPage/SponsorPage.jsx
@@ -15,40 +15,39 @@ const SponsorPage = () => {
           width: '100%',
           justifyContent: 'start',
           alignItems: 'center',
-          height: 'fit-content',
-          background: 'linear-gradient(var(--red), var(--black))'
+          background: 'linear-gradient(var(--red), var(--black))',
         }}
       >
         <h2
           style={{
             fontFamily: 'Mokoto',
             color: '#FFFFFF',
-            textAlign: 'center'
+            textAlign: 'center',
           }}
-            className="my-5"
+          className="my-5"
         >
           Thank you to our sponsors that make HackRPI possible!
         </h2>
-        
+
         {sponsorGroups.sponsorGroups.map((tempSponsorGroup, indx) => {
-          const tierList = ["Obsidian", "Gold", "Silver", "Bronze", "Collaborators"]
-          return(
-            <div className="container" key={"sponsorGroup"+indx}>
-              <div className="row g-2">
-                <h3 className="mokoto tierHeader">{tierList[indx]}</h3>
+          const tierList = ["Obsidian", "Gold", "Silver", "Bronze", "Collaborators"];
+          return (
+            <div className="container" key={"sponsorGroup" + indx}>
+              <h3 className="mokoto tierHeader">{tierList[indx]}</h3>
+              <div className="d-flex justify-content-center flex-wrap">
                 {tempSponsorGroup.map((sponsorGroup, innerIndx) => (
-                  <div className="sponsor col-md-3 d-flex mb-5" key={"sponsorGroup"+indx+"sponsor"+innerIndx}>
+                  <div className="sponsor col-md-3 d-flex mb-5" key={"sponsorGroup" + indx + "sponsor" + innerIndx}>
                     <a href={sponsorGroup.url} target="_blank" className="rounded px-3">
-                          <div className="d-flex flex-column align-items-center justify-content-center innerSponsor">
-                            <img src={sponsorGroup.logoPath} className="img-fluid rounded mw-100 mh-100"  alt={sponsorGroup.name}/>
-                        </div>
+                      <div className="d-flex flex-column align-items-center justify-content-center innerSponsor">
+                        <img src={sponsorGroup.logoPath} className="img-fluid rounded mw-100 mh-100" alt={sponsorGroup.name} />
+                      </div>
                     </a>
                   </div>
                 ))}
               </div>
             </div>
-            )
-          })}
+          );
+        })}
       </div>
     </>
   );
