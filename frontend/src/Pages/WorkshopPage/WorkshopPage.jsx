226c8c3591dc2fb78930ce13800c8a2d867095ac
ishipandey20 <pandei@rpi.edu>
2023-10-31 17:34:53 -0400

    Added more workshops
    added more workshops and edited information in the json
    
    Co-Authored-By: andrewtarnavsky <145161755+andrewtarnavsky@users.noreply.github.com>

--- frontend/src/Pages/WorkshopPage/WorkshopPage.jsx
+++ frontend/src/Pages/WorkshopPage/WorkshopPage.jsx
@@ -25,9 +25,30 @@ const WorkshopPage = () => {
 
     const workshops = [
         {
+            title: 'Patient Safety 101',
+            time: '12-12:30pm',
+            location: 'DCC 318',
+            speaker: 'TBD',
+            pic: workshopSafety,
+            description: 'This workshop is designed for hackers interested in the Patient Safety Tech Prize. It will\
+            give you background on patient safety as an urgent issue and will help inspire you to\
+            spend your weekend hacking together a project that has the potential to save lives! \
+            The team with the best tech-enabled solution will win $1000.'
+        },
+
+        {
+          title: 'Intro to Tech Stacks',
+          time: '12:30-1:30pm',
+          location: 'DCC 308',
+          speaker: 'Michael and Kuzmin',
+          pic: workshopSafety,
+          description: 'TBD'
+        },
+        
+        {
             title: 'Intro to MERN Stack Web Dev Workshop',
             time: '1:30-2:30pm',
-            location: 'TBD',
+            location: 'DCC 308',
             speaker: 'Yash Kaul',
             pic: workshopMern,
             description: 'Gain hands-on experience with MongoDB—covering schema validation, client to database connection, and implementing API \
@@ -36,74 +57,38 @@ const WorkshopPage = () => {
              and build/run/test your application easily.  This workshop aims to introduce the basics of MERN applications for use in the\
              hackathon and discuss potential considerations for real world use and deployment.'
         },
-        
+
         {
-            title: 'Password Exploitation - FBI Albany',
-            time: '5:30-6:30pm',
-            location: 'TBD',
-            speaker: 'Alex Vargas and Ryan Gallagher',
-            pic: workshopPassword,
-            description: 'Join the FBI as they explain the math behind password complexity, including tactics for how to attack and\
-            break passwords. They will cover steps on how to extract a password hash from a file and run it with \
-           hashcat to crack the password. '
+          title: 'Mobile Dev Workshop',
+          time: '1:30-2:30pm',
+          location: 'DCC 318',
+          speaker: 'Brian Ingmanson',
+          pic: workshopMern,
+          description: "If you've ever wondered how those awesome apps on your phone are made, or if you've got an itch to create something fun and \
+          innovative, the Mobile Dev Workshop is the perfect place to start! The main course of our workshop revolves around React and Flutter.  \
+          These two powerful frameworks empower you to create fantastic, user-friendly, and visually stunning apps for both IOS and Android at  \
+          once. We also will explore Google Firebase, a service that lets you effortlessly manage user data, authentication, and real-time updates.\
+          With this software, you can quickly and effectively deploy fully functioning apps with minimal hardships. \
+          But what's learning without doing? We've got an exciting coding activity for you! Get ready to roll up your \
+          sleeves as you embark on a mini-project to create your mobile app. You'll apply everything you've \
+          learned in a fun, hands-on session, and you might come up with the next big app idea!\
+          No experience? No problem! This workshop is designed for beginners, and we will\
+          be there every step of the way to help you get everything set up."
         },
 
         {
-            title: 'Hacking the machine for fun (and sometimes profit)',
-            time: '4:30-5:30pm',
-            location: 'TBD',
-            speaker: ' Dr. Brian Callahan (w/ Ayah Tharwat )',
-            pic: workshopHacking,
-            description: 'Securing software requires developers to learn how to think like an attacker; you cannot defend against what you cannot\
-            imagine going  wrong with your software. There are a wide variety of potential attacks, but \
-            learning just a basic few can go a very long way towards improving your coding skills. In this workshop, we will participate in a \
-            set of interactive Capture the Flag (CTF) challenges designed to demonstrate some of the \
-            ways attackers think. Alongside hacking our machines, we will also learn some secure coding paradigms \
-            to protect against these attacks. By the end of the workshop, you will have a newfound appreciation for the importance \
-            of secure coding and be armed with the ability to build more secure software.'
-        },
-
-        {
-            title: 'Patient Safety 101',
-            time: '12-12:30pm',
-            location: 'TBD',
-            speaker: 'TBD',
-            pic: workshopSafety,
-            description: 'This workshop is designed for hackers interested in the Patient Safety Tech Prize. It will\
-            give you background on patient safety as an urgent issue and will help inspire you to\
-            spend your weekend hacking together a project that has the potential to save lives! \
-           The team with the best tech-enabled solution will win $1000.'
-        },
-
-        {
-            title: 'Design Thinking for Technical Innovation',
-            time: '4:30-5:30pm',
-            location: 'TBD',
-            speaker: 'Kylinn Askew',
-            pic: workshopDesign,
-            description: 'Explore the power of design thinking to drive innovation and foster collaboration to build\
-            strong technical products and startups. Learn how to harness creative problem-solving,\
-            empathetic user-centric approaches, and cross-functional teamwork to create products\
-            that not only meet technical requirements but also exceed user expectations'
-        },
-
-        {
-            title: 'AI made easy with Hugging Face',
-            time: '6:30-7:30pm',
-            location: 'TBD',
-            speaker: 'Raven Levitt',
-            pic: workshopAI,
-            description: 'Have a hack that needs AI but no idea where to start? In this workshop I’ll go over how to use the\
-            Hugging Face API to easily leverage thousands of different machine learning models to tackle whatever\
-             challenge you’re dealing with, be it text-generation, object detection, or just about any other machine \
-            learning challenge, hugging face is a great place to start! After a short presentation we’ll hold a mini \
-            competition for the best use of the API, and there just might be prizes…'
+          title: "A beginner's Guide to Quantum Computing",
+          time: '2:30-3:30pm',
+          location: 'DCC 308',
+          speaker: 'Brian Ingmanson',
+          pic: workshopMern,
+          description: 'How folks can run their first quantum circuit on a real quantum computer, and some of the quirks of quantum computing.'
         },
 
         {
           title: 'Agile Development Workshop',
           time: '3:30-4:30pm',
-          location: 'TBD',
+          location: 'DCC 327',
           speaker: 'John Sturman',
           pic: workshopAgile,
           description: 'Join John Sturman for a presentation about agile development and how to work in a development group for maximum productivity.\
@@ -112,9 +97,60 @@ const WorkshopPage = () => {
       },
 
       {
+        title: 'Hacking the machine for fun (and sometimes profit)',
+        time: '4:30-5:30pm',
+        location: 'DCC 318',
+        speaker: ' Dr. Brian Callahan (w/ Ayah Tharwat )',
+        pic: workshopHacking,
+        description: 'Securing software requires developers to learn how to think like an attacker; you cannot defend against what you cannot\
+        imagine going  wrong with your software. There are a wide variety of potential attacks, but \
+        learning just a basic few can go a very long way towards improving your coding skills. In this workshop, we will participate in a \
+        set of interactive Capture the Flag (CTF) challenges designed to demonstrate some of the \
+        ways attackers think. Alongside hacking our machines, we will also learn some secure coding paradigms \
+        to protect against these attacks. By the end of the workshop, you will have a newfound appreciation for the importance \
+        of secure coding and be armed with the ability to build more secure software.'
+    },
+
+    {
+      title: 'Design Thinking for Technical Innovation',
+      time: '4:30-5:30pm',
+      location: 'DCC 308',
+      speaker: 'Kylinn Askew',
+      pic: workshopDesign,
+      description: 'Explore the power of design thinking to drive innovation and foster collaboration to build\
+      strong technical products and startups. Learn how to harness creative problem-solving,\
+      empathetic user-centric approaches, and cross-functional teamwork to create products\
+      that not only meet technical requirements but also exceed user expectations'
+    },
+
+    {
+        title: 'Password Exploitation - FBI Albany',
+        time: '5:30-6:30pm',
+        location: 'DCC 318',
+        speaker: 'Alex Vargas and Ryan Gallagher',
+        pic: workshopPassword,
+        description: 'Join the FBI as they explain the math behind password complexity, including tactics for how to attack and\
+        break passwords. They will cover steps on how to extract a password hash from a file and run it with \
+        hashcat to crack the password. '
+    },
+
+    {
+        title: 'AI made easy with Hugging Face',
+        time: '6:30-7:30pm',
+        location: 'DCC 327',
+        speaker: 'Raven Levitt',
+        pic: workshopAI,
+        description: 'Have a hack that needs AI but no idea where to start? In this workshop I’ll go over how to use the\
+        Hugging Face API to easily leverage thousands of different machine learning models to tackle whatever\
+          challenge you’re dealing with, be it text-generation, object detection, or just about any other machine \
+        learning challenge, hugging face is a great place to start! After a short presentation we’ll hold a mini \
+        competition for the best use of the API, and there just might be prizes…'
+    },
+
+      {
         title: 'Sticker Workshop with The Forge',
-        time: 'TBD',
-        location: 'TBD',
+        time: '7:30-8:30',
+        location: 'Forge',
         speaker: 'Mikiel Gica, Saad Mujahid, and Julia Kapchynsky',
         pic: workshopForge,
         description: 'The Forge will be open to anyone interested in designing and creating their\
@@ -124,7 +160,6 @@ const WorkshopPage = () => {
         and a few images you want to turn into stickers is strongly recommended \
        to be able to follow along and participate.'
     },
-        // Add more workshop objects as needed
     ];
 
     
