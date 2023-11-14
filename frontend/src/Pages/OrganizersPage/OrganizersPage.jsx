bb64e74fe11467cd7298d54902ecc21445e317e4
alin600 <alilin.al6@gmail.com>
2023-11-14 12:24:56 -0500

    adding discord button

--- frontend/src/Pages/OrganizersPage/OrganizersPage.jsx
+++ frontend/src/Pages/OrganizersPage/OrganizersPage.jsx
@@ -26,207 +26,201 @@ import amandaImage from './../../../assets/TeamPics/amanda image.jpeg';
 import cooperImage from './../../../assets/TeamPics/cooper image.jpg';
 
 const OrganizersPage = () => {
-  const teamMembersFirstRow = [
-    {
-      name: 'Maryellen Marino',
-      role: 'President',
-      image: maryellenImage,
-      avatar: maryellenAvatar,
-    },
-    {
-      name: 'Raven Levitt',
-      role: 'Vice President',
-      image: ravenImage,
-      avatar: ravenAvatar,
-    },
-    {
-      name: 'John Kloepfer',
-      role: 'Director of Technology',
-      image: johnImage,
-      avatar: johnAvatar,
-    },
-  ];
+    const teamMembersFirstRow = [
+        {
+            name: 'Maryellen Marino',
+            role: 'President',
+            image: maryellenImage,
+            avatar: maryellenAvatar,
+        },
+        {
+            name: 'Raven Levitt',
+            role: 'Vice President',
+            image: ravenImage,
+            avatar: ravenAvatar,
+        },
+        {
+            name: 'John Kloepfer',
+            role: 'Director of Technology',
+            image: johnImage,
+            avatar: johnAvatar,
+        },
+    ];
 
-  const teamMembersSecondRow = [
-    {
-      name: 'Xenia Khusid',
-      role: 'Director of Logistics',
-      image: xeniaImage,
-      avatar: xeniaAvatar,
-    },
-    {
-      name: 'Adwait Naware',
-      role: 'Director of Finance',
-      image: adwaitImage,
-      avatar: adwaitAvatar,
-    },
-    {
-      name: 'Sanay Tralshawala',
-      role: 'Director of Sponsorship',
-      image: sanayImage,
-      avatar: sanayAvatar,
-    },
-    {
-      name: 'Miranda Zheng',
-      role: 'Director of Marketing',
-      image: mirandaImage,
-      avatar: mirandaAvatar,
-    },
-  ];
+    const teamMembersSecondRow = [
+        {
+            name: 'Xenia Khusid',
+            role: 'Director of Logistics',
+            image: xeniaImage,
+            avatar: xeniaAvatar,
+        },
+        {
+            name: 'Adwait Naware',
+            role: 'Director of Finance',
+            image: adwaitImage,
+            avatar: adwaitAvatar,
+        },
+        {
+            name: 'Sanay Tralshawala',
+            role: 'Director of Sponsorship',
+            image: sanayImage,
+            avatar: sanayAvatar,
+        },
+        {
+            name: 'Miranda Zheng',
+            role: 'Director of Marketing',
+            image: mirandaImage,
+            avatar: mirandaAvatar,
+        },
+    ];
 
-  const jrTeamMembersFirstRow = [
-    {
-      name: 'CJ Marino',
-      role: 'Junior Director of Logistics',
-      image: cjImage,
-    },
-    {
-      name: 'Grace Hui',
-      role: 'Junior Director of Finance',
-      image: graceImage,
-    },
-  ];
+    const jrTeamMembersFirstRow = [
+        {
+            name: 'CJ Marino',
+            role: 'Junior Director of Logistics',
+            image: cjImage,
+        },
+        {
+            name: 'Grace Hui',
+            role: 'Junior Director of Finance',
+            image: graceImage,
+        },
+    ];
 
-  const jrTeamMembersSecondRow = [
-    {
-      name: 'Heman Kolla',
-      role: 'Junior Director of Sponsorship',
-      image: hemanImage,
-    },
-    {
-      name: 'Amanda Ruan',
-      role: 'Junior Director of Marketing',
-      image: amandaImage,
-    },
-    {
-      name: 'Cooper Werner',
-      role: 'Junior Director of Technology',
-      image: cooperImage,
-    },
-  ];
+    const jrTeamMembersSecondRow = [
+        {
+            name: 'Heman Kolla',
+            role: 'Junior Director of Sponsorship',
+            image: hemanImage,
+        },
+        {
+            name: 'Amanda Ruan',
+            role: 'Junior Director of Marketing',
+            image: amandaImage,
+        },
+        {
+            name: 'Cooper Werner',
+            role: 'Junior Director of Technology',
+            image: cooperImage,
+        },
+    ];
 
-  return (
-    <div id="members" className="MeetTheTeamPage centeredText">
-      <h1 id="meet-our-team-header" style={{ fontFamily: 'Mokoto', fontSize: '46px' }}>
-        Meet Our Team!
-      </h1>
+    return (
+        <div id="members" className="OrganizersPage centeredText">
+            <div className="help-organize-section">
+                <h1 style={{ fontFamily: 'Mokoto', fontSize: '46px', marginBottom: '20px' }}>
+                    Help Organize
+                </h1>
+            </div>
 
-      {/* Directors: */}
+            {/* Move the button outside the help-organize-section div */}
+            <Nav.Link href="https://discord.gg/mjsusWUM">
+                <Button variant="outline" className='button' style={{ backgroundColor: '#910307', color: 'white', marginBottom: '30px', }}>
+                    Join Now!
+                </Button>
+            </Nav.Link>
+            <h1 id="organizers-header" style={{ fontFamily: 'Mokoto', fontSize: '46px' }}>
+                Meet Our Team!
+            </h1>
 
-      <div className="team-members">
-        <h1 className="director-jrDirector-header" style={{ padding: '0 7px' }}>
-          Meet The <a style={{ color: '#910307' }}>Directors</a>
-          <p style={{ fontSize: '10px', fontStyle: 'italic' }}>Avatars by Lilu Kaufman</p>
-        </h1>
+            {/* Directors: */}
+
+            <div className="team-members">
+                <h1 className="director-jrDirector-header" style={{ padding: '0 7px' }}>
+                    Meet The <a style={{ color: '#910307' }}>Directors</a>
+                    <p style={{ margin: '10px', fontSize: '10px', fontStyle: 'italic' }}>Avatars by Lilu Kaufman</p>
+                </h1>
 
 
-        <Row className="director-row">
-          {/* First Row */}
-          {teamMembersFirstRow.map((member, index) => (
-            <Col key={index} md={3}>
-              <div className="director-div">
-                <div style={{ width: 'auto', height: 'auto', display: 'grid', justifySelf: 'center', alignSelf: 'center' }}>
-                  <img className='director-img' src={member.image} alt={member.name}/>
-                  <img className='director-avatar' src={member.avatar} alt={member.name}/>
-                </div>
-                <div className="director-name-div">
-                  <h2 className="name">{member.name}</h2>
-                  <p className="position">{member.role}</p>
-                  {/* <Button className="button" style={{ marginBottom: '10px' }}>
+                <Row className="director-row">
+                    {/* First Row */}
+                    {teamMembersFirstRow.map((member, index) => (
+                        <Col key={index} md={3}>
+                            <div className="director-div">
+                                <div style={{ width: 'auto', height: 'auto', display: 'grid', justifySelf: 'center', alignSelf: 'center' }}>
+                                    <img className='director-img' src={member.image} alt={member.name} />
+                                    <img className='director-avatar' src={member.avatar} alt={member.name} />
+                                </div>
+                                <div className="director-name-div">
+                                    <h2 className="name">{member.name}</h2>
+                                    <p className="position">{member.role}</p>
+                                    {/* <Button className="button" style={{ marginBottom: '10px' }}>
                     More Info
                   </Button> */}
-                </div>
-              </div>
-            </Col>
-          ))}
-        </Row>
+                                </div>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
 
-        <Row className="director-row" style={{ columnGap: '0' }}>
-          {/* Second Row */}
-          {teamMembersSecondRow.map((member, index) => (
-            <Col key={index} md={3}>
-              <div className="director-div">
-                <div style={{ width: 'auto', height: 'auto', display: 'grid', justifySelf: 'center', alignSelf: 'center' }}>
-                  <img className='director-img' src={member.image} alt={member.name}/>
-                  <img className='director-avatar' src={member.avatar} alt={member.name}/>
-                </div>
-                <div className="director-name-div">
-                  <h2 className="name">{member.name}</h2>
-                  <p className="position">{member.role}</p>
-                  {/* <Button className="button" style={{ marginBottom: '10px' }}>
+                <Row className="director-row" style={{ columnGap: '0' }}>
+                    {/* Second Row */}
+                    {teamMembersSecondRow.map((member, index) => (
+                        <Col key={index} md={3}>
+                            <div className="director-div">
+                                <div style={{ width: 'auto', height: 'auto', display: 'grid', justifySelf: 'center', alignSelf: 'center' }}>
+                                    <img className='director-img' src={member.image} alt={member.name} />
+                                    <img className='director-avatar' src={member.avatar} alt={member.name} />
+                                </div>
+                                <div className="director-name-div">
+                                    <h2 className="name">{member.name}</h2>
+                                    <p className="position">{member.role}</p>
+                                    {/* <Button className="button" style={{ marginBottom: '10px' }}>
                     More Info
                   </Button> */}
-                </div>
-              </div>
-            </Col>
-          ))}
-        </Row>
+                                </div>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
 
-        {/* Jr Directors: */}
+                {/* Jr Directors: */}
 
-        <h1 className="director-jrDirector-header" style={{ marginTop: '25px' }}>
-          Meet the <a style={{ color: '#d50110' }}>Junior Directors</a>
-        </h1>
-        <Row className="director-row jr">
-          {/* First Row */}
-          {jrTeamMembersFirstRow.map((member, index) => (
-            <Col key={index} md={3}>
-              <div className="director-div">
-                <div style={{ width: 'auto', height: 'auto', display: 'grid', justifySelf: 'center', alignSelf: 'center' }}>
-                  <img className='director-img' src={member.image} alt={member.name}/>
-                </div>
-                <div className="director-name-div">
-                  <h2 className="name">{member.name}</h2>
-                  <p className="position junior">{member.role}</p>
-                  {/* <Button className="button" style={{ marginBottom: '10px' }}>
+                <h1 className="director-jrDirector-header" style={{ marginTop: '25px' }}>
+                    Meet the <a style={{ color: '#d50110' }}>Junior Directors</a>
+                </h1>
+                <Row className="director-row jr">
+                    {/* First Row */}
+                    {jrTeamMembersFirstRow.map((member, index) => (
+                        <Col key={index} md={3}>
+                            <div className="director-div">
+                                <div style={{ width: 'auto', height: 'auto', display: 'grid', justifySelf: 'center', alignSelf: 'center' }}>
+                                    <img className='director-img' src={member.image} alt={member.name} />
+                                </div>
+                                <div className="director-name-div">
+                                    <h2 className="name">{member.name}</h2>
+                                    <p className="position junior">{member.role}</p>
+                                    {/* <Button className="button" style={{ marginBottom: '10px' }}>
                     More Info
                   </Button> */}
-                </div>
-              </div>
-            </Col>
-          ))}
-        </Row>
+                                </div>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
 
-        <Row className="director-row jr">
-          {/* Second Row */}
-          {jrTeamMembersSecondRow.map((member, index) => (
-            <Col key={index} md={3}>
-              <div className="director-div">
-                <div style={{ width: 'auto', height: 'auto', display: 'grid', justifySelf: 'center', alignSelf: 'center' }}>
-                  <img className='director-img' src={member.image} alt={member.name}/>
-                </div>
-                <div className="director-name-div">
-                  <h2 className="name">{member.name}</h2>
-                  <p className="position junior">{member.role}</p>
-                  {/* <Button className="button" style={{ marginBottom: '10px' }}>
+                <Row className="director-row jr">
+                    {/* Second Row */}
+                    {jrTeamMembersSecondRow.map((member, index) => (
+                        <Col key={index} md={3}>
+                            <div className="director-div">
+                                <div style={{ width: 'auto', height: 'auto', display: 'grid', justifySelf: 'center', alignSelf: 'center' }}>
+                                    <img className='director-img' src={member.image} alt={member.name} />
+                                </div>
+                                <div className="director-name-div">
+                                    <h2 className="name">{member.name}</h2>
+                                    <p className="position junior">{member.role}</p>
+                                    {/* <Button className="button" style={{ marginBottom: '10px' }}>
                     More Info
                   </Button> */}
-                </div>
-              </div>
-            </Col>
-          ))}
-        </Row>
-
-        {/* Tab content and buttons */}
-        <p style={{fontSize: '2em', fontWeight: 'bold'}}>
-          Want to Help?
-        </p>
-
-        <div className="button-container">
-          <Nav.Link href="https://forms.gle/VY1vXF1mPMgx82wdA">
-            <Button variant="outline"className='button'style={{ backgroundColor: '#910307', color: 'white' }}>
-              Mentor Application
-            </Button>
-          </Nav.Link>
-          <Nav.Link href="https://forms.gle/kEpdKKKrFsSUQztR6">
-            <Button variant="outline" className='button'style={{ backgroundColor: '#910307', color: 'white', marginLeft: '30px' }}>
-              Volunteer Application
-            </Button>
-          </Nav.Link>
+                                </div>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 
