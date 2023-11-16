62264824c6fb2040c0394bee8ac0320335b3f901
ths22115 <xselymx@gmail.com>
2023-11-15 21:52:54 -0500

    tweaked button styling and cleaned up code

--- frontend/src/Pages/ThankYouPage/ThankYouPage.jsx
+++ frontend/src/Pages/ThankYouPage/ThankYouPage.jsx
@@ -63,44 +63,22 @@ const ThankYouPage = () => {
 
   return (
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', width: '100%', height: '100%', position: 'fixed'}}>
-      <div className="thank-you-page text-center">
-        {/*<h1>*/}
-        {/*<div className="thankyou-text">*/}
-        {/*  Thank you <br />*/}
-        {/*  </div>*/}
-        {/*  <span className="small-text">*/}
-        {/*    for joining us at <br /> HACKRPI X!*/}
-        {/*  </span>*/}
-        {/*</h1>*/}
 
-        {/*font-family: Poppins Light;*/}
-        {/*font-size: 50px;*/}
-
-      </div>
-      <h1 style={{ margin: '30px 0 -35px 0', fontFamily: 'Mokoto', fontSize: '210px'}}>THANK YOU</h1>
+      <h1 style={{ margin: '30px 0 -35px 0', fontFamily: 'Mokoto', fontSize: '200px'}}>THANK YOU</h1>
       <h1 style={{ fontFamily: 'Mokoto', color: '#910307' ,fontSize: '110px', margin: '0 0 -15px 0' }}>HACKERS</h1>
       <h2 style={{ fontFamily: 'Poppins', fontSize: '38px', marginTop: '37px' }}>for
         <a style={{ fontFamily: 'Mokoto', fontSize: '42px', color: '#910307' }}> Changing The World </a>
         with us!
       </h2>
-      {/*<div className="button-container">*/}
-      {/*  <LinkContainer to="/home">*/}
-      {/*    <Nav.Link className="button">*/}
-      {/*      Back to HackRPI X*/}
-      {/*    </Nav.Link>*/}
-      {/*  </LinkContainer>*/}
-      {/*</div>*/}
-      {/*<div className="centered-button-container">*/}
-      {/*  <LinkContainer to="/winners">*/}
-      {/*    <Nav.Link className="button">*/}
-      {/*      Check out our Winners!*/}
-      {/*    </Nav.Link>*/}
-      {/*  </LinkContainer>*/}
-      {/*</div>*/}
-      <div id='button-container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', zIndex: '5', marginTop: '113px', rowGap: '35px'}}>
+
+      <div id='button-container'
+           style={{
+             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', zIndex: '5',
+             rowGap: '45px', position: 'absolute', bottom: '150px'
+      }}>
         <LinkContainer to="/winners">
           <Nav.Link id='winners-button' className="button">
-            Our Winners
+            See Our Winners!
           </Nav.Link>
         </LinkContainer>
         <div id='link-button-container' style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', columnGap: '20px'}}>
