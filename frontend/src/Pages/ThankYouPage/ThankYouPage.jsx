4a46c32e6e123c6f899fc891ed8252fb898bb610
ths22115 <xselymx@gmail.com>
2023-11-15 21:39:50 -0500

    Text content and buttons added and styled

--- frontend/src/Pages/ThankYouPage/ThankYouPage.jsx
+++ frontend/src/Pages/ThankYouPage/ThankYouPage.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import './ThankYouPage.css';
+import '../../fonts.css'
 import Globe from 'react-globe.gl';
 import data from '../../../assets/data.geojson';
 import globeimage from '../../../assets/earth-dark.jpg';
@@ -57,11 +58,11 @@ const ThankYouPage = () => {
     globeEl.current.pointOfView(mapCenter, transitionSpeed);
   }, []);
 
-  let globeWidth = windowDimensions.width * 1.6;
-  let globeHeight = windowDimensions.height * 1.6;
+  let globeWidth = windowDimensions.width * 1.75;
+  let globeHeight = windowDimensions.height * 1.75;
 
   return (
-    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%', position: 'fixed'}}>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', width: '100%', height: '100%', position: 'fixed'}}>
       <div className="thank-you-page text-center">
         {/*<h1>*/}
         {/*<div className="thankyou-text">*/}
@@ -71,7 +72,17 @@ const ThankYouPage = () => {
         {/*    for joining us at <br /> HACKRPI X!*/}
         {/*  </span>*/}
         {/*</h1>*/}
+
+        {/*font-family: Poppins Light;*/}
+        {/*font-size: 50px;*/}
+
       </div>
+      <h1 style={{ margin: '30px 0 -35px 0', fontFamily: 'Mokoto', fontSize: '210px'}}>THANK YOU</h1>
+      <h1 style={{ fontFamily: 'Mokoto', color: '#910307' ,fontSize: '110px', margin: '0 0 -15px 0' }}>HACKERS</h1>
+      <h2 style={{ fontFamily: 'Poppins', fontSize: '38px', marginTop: '37px' }}>for
+        <a style={{ fontFamily: 'Mokoto', fontSize: '42px', color: '#910307' }}> Changing The World </a>
+        with us!
+      </h2>
       {/*<div className="button-container">*/}
       {/*  <LinkContainer to="/home">*/}
       {/*    <Nav.Link className="button">*/}
@@ -86,8 +97,26 @@ const ThankYouPage = () => {
       {/*    </Nav.Link>*/}
       {/*  </LinkContainer>*/}
       {/*</div>*/}
-      <div className="globe" style={{width: 'auto', height: 'auto', position: 'absolute', bottom: '0', transform: 'translate(0%, 66%)',
-        overflow:'hidden', zIndex: '1'}}>
+      <div id='button-container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', zIndex: '5', marginTop: '113px', rowGap: '35px'}}>
+        <LinkContainer to="/winners">
+          <Nav.Link id='winners-button' className="button">
+            Our Winners
+          </Nav.Link>
+        </LinkContainer>
+        <div id='link-button-container' style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', columnGap: '20px'}}>
+          <LinkContainer to="/home">
+            <Nav.Link className="button">
+              Back To HackRPI X
+            </Nav.Link>
+          </LinkContainer>
+            <Nav.Link href='https://tinyurl.com/hackrpi-donation' className="button">
+              Support Our Next Event
+            </Nav.Link>
+        </div>
+      </div>
+      <div className="globe" style={{width: 'auto', height: 'auto',
+        position: 'absolute', bottom: '0', transform: 'translate(0%, 66%)'
+      }}>
         <Globe id='earth'
                ref={globeEl}
                width={globeWidth}
@@ -98,8 +127,8 @@ const ThankYouPage = () => {
                backgroundColor={'rgb(255,255,255, 0)'}
                polygonsData={countries.features.filter((d) => d.properties.ISO_A2 !== 'AQ')}
                polygonAltitude={altitude}
-               polygonCapColor={() => 'rgba(150, 0, 0, 0.8)'}
-               polygonSideColor={() => 'rgba(255, 255, 255, 0.10)'}
+               polygonCapColor={() => 'rgba(145,3,7,.8)'}
+               polygonSideColor={() => 'rgba(255, 255, 255,0.10)'}
                polygonsTransitionDuration={500}
         />
       </div>
