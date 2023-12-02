e377e2133d141c588546cfa869c4dc45583a6e21
EURICIA <xinchaochen1095@gmail.com>
2023-12-01 20:46:43 -0500

    Update ugliest-code-competition.jsx
    Adjusted the formatting of the page. For now have css on the same page because of routing issues. Also added a function that shows a small window that will be use to present interesting code when specific winner is clicked (ie first place).

--- frontend/src/Pages/Ugliest code competiton/ugliest-code-competition.jsx
+++ frontend/src/Pages/Ugliest code competiton/ugliest-code-competition.jsx
@@ -1,30 +1,97 @@
-import React from 'react';
-import './styles.css'
+import React, { useState } from 'react';
 
 const UgliestCodeCompetition = () => {
-  // Define prizes as a JSON object
   const prizes = [
-    { place: 'First Place Award', prize: 'An iPhone 15pro' },
+    { place: 'First Place Award', prize: 'An iPhone 15 Pro' },
     { place: 'Second Place Award', prize: 'An AirPods Max' },
     { place: 'Third Place Award', prize: 'An Apple Watch Series 9' },
   ];
 
-  return (
+  const [showWindow, setShowWindow] = useState(false);
+  const [selectedPrize, setSelectedPrize] = useState({});
 
-      <section id="ugliest-code-competition" style={{ padding: '20px' }}>
-        <h1 style={{ color: '#f50057' }}>UGLIEST CODE COMPETITION</h1>
-        <p>An "Ugliest Code Competition" is a humorous contest where programmers intentionally create the most convoluted and poorly written code.</p>
-        <button style={{ backgroundColor: '#f50057', color: '#fff', padding: '10px', border: 'none', cursor: 'pointer' }}>Join now</button>
-        {/* Dynamically render prizes */}
-        <div className="awards" style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
-          {prizes.map((prize, index) => (
-            <article key={index} style={{ flex: 1, marginRight: '10px' }}>
-              <h2 style={{ color: '#f50057' }}>{prize.place}</h2>
-              <p>{prize.prize}</p>
-            </article>
-          ))}
+  const sectionStyle = {
+    padding: '20px',
+    background: 'url("unset") no-repeat center center',
+    backgroundSize: 'cover',
+    textAlign: 'center',
+    position: 'relative',
+  };
+
+  const h1Style = {
+    color: '#f50057',
+    fontSize: '2.5em',
+  };
+
+  const buttonStyle = {
+    backgroundColor: '#f50057',
+    color: '#fff',
+    padding: '10px',
+    border: 'none',
+    cursor: 'pointer',
+    marginTop: '20px',
+  };
+
+  const awardsStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: '20px',
+  };
+
+  const articleStyle = {
+    flex: 1,
+    marginRight: '10px',
+    border: '1px solid #eee',
+    padding: '10px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  };
+
+  const popupStyle = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    background: '#fff',
+    padding: '20px',
+    borderRadius: '5px',
+    boxShadow: '0 0 20px rgba(0, 0, 0, 0.3)',
+    zIndex: 9999, // Highest priority
+  };
+
+  const h2Style = {
+    color: '#f50057',
+    marginBottom: '10px',
+  };
+
+  const toggleWindow = (prize) => {
+    setShowWindow(!showWindow);
+    setSelectedPrize(prize);
+  };
+
+  return (
+    <section id="ugliest-code-competition" style={sectionStyle}>
+      <h1 style={h1Style}>UGLIEST CODE COMPETITION</h1>
+      <p>An "Ugliest Code Competition" is a humorous contest where programmers intentionally create the most convoluted and poorly written code.</p>
+      <button style={buttonStyle} onClick={() => toggleWindow({})}>
+        Join now
+      </button>
+      <div className="awards" style={awardsStyle}>
+        {prizes.map((prize, index) => (
+          <article key={index} style={articleStyle} onClick={() => toggleWindow(prize)}>
+            <h2 style={h2Style}>{prize.place}</h2>
+            <p>{prize.prize}</p>
+          </article>
+        ))}
+      </div>
+      {showWindow && (
+        <div style={popupStyle}>
+          <h2>{selectedPrize.place}</h2>
+          <p>{selectedPrize.prize}</p>
+          <button onClick={() => setShowWindow(false)}>Close</button>
         </div>
-      </section>
+      )}
+    </section>
   );
 };
 
