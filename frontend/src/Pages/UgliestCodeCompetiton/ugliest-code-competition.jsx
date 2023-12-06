6e6d0bf5f846ae50128b064ea0bd93e51d7237b2
TouHouPeace <143843739+TouHouPeace@users.noreply.github.com>
2023-12-05 23:29:54 -0500

    Attemp # I don't know

--- frontend/src/Pages/UgliestCodeCompetiton/ugliest-code-competition.jsx
+++ frontend/src/Pages/UgliestCodeCompetiton/ugliest-code-competition.jsx
@@ -43,19 +43,67 @@ const UgliestCodeCompetition = () => {
     },
   ];
 
-  const [selectedPrizeIndex, setSelectedPrizeIndex] = useState(null);
   const [showWindows, setShowWindows] = useState(Array(ugliestCodeArray.length).fill(false));
+  const [selectedPrizeIndex, setSelectedPrizeIndex] = useState(null);
 
   const sectionStyle = {
-    // ... (unchanged)
+    padding: '20px',
+    background: 'url("unset") no-repeat center center',
+    backgroundSize: 'cover',
+    textAlign: 'center',
+    position: 'relative',
+    maxWidth: '800px', 
+    margin: '0 auto', 
   };
 
-  // ... (unchanged)
+  const h1Style = {
+    color: '#f50057',
+    fontSize: '2.5em',
+    marginBottom: '10px', 
+  };
+
+  const pStyle = {
+    fontSize: '1.2em',
+    color: 'gray', 
+    maxWidth: '600px', 
+    margin: '0 auto', 
+    lineHeight: '1.5',
+  };
+
+  const buttonStyle = {
+    backgroundColor: 'white',
+    color: 'rgb(145, 3, 7)',
+    padding: '10px',
+    border: 'none',
+    cursor: 'pointer',
+    marginTop: '20px',
+    borderRadius: '5px',
+  };
+
+  const awardsStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: '20px',
+  };
+
+  const articleStyle = {
+    flex: 1,
+    marginRight: '10px',
+    border: '1px solid #eee',
+    padding: '10px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  };
+
+  const h2Style = {
+    color: '#f50057',
+    marginBottom: '10px',
+  };
 
   const toggleWindow = (index) => {
     setShowWindows((prevShowWindows) => {
-      const newShowWindows = [...prevShowWindows];
-      newShowWindows[index] = !newShowWindows[index];
+      const newShowWindows = Array(ugliestCodeArray.length).fill(false);
+      newShowWindows[index] = !prevShowWindows[index];
       return newShowWindows;
     });
 
@@ -64,14 +112,30 @@ const UgliestCodeCompetition = () => {
 
   return (
     <section id="ugliest-code-competition" style={sectionStyle}>
-      {/* ... (unchanged) */}
+      <h1 style={h1Style}>UGLIEST CODE COMPETITION</h1>
+      <p style={pStyle}>
+        An "Ugliest Code Competition" is a humorous contest where programmers intentionally create
+        the most convoluted and poorly written code.
+      </p>
+      <button style={buttonStyle} onClick={() => toggleWindow(0)}>
+        Join now
+      </button>
+      <div className="awards" style={awardsStyle}>
+        {prizes.map((prize, index) => (
+          <article key={index} style={articleStyle} onClick={() => toggleWindow(index)}>
+            <h2 style={h2Style}>{prize.place}</h2>
+            <p>{prize.prize}</p>
+          </article>
+        ))}
+      </div>
+
       {showWindows.map((showWindow, index) => (
         showWindow && (
           <CodePopup
-            key={index}
-            title={ugliestCodeArray[selectedPrizeIndex].title}
+            key={index} // Use a unique key based on your data structure
+            title={ugliestCodeArray[index].title}
             onClose={() => toggleWindow(index)}
-            code={ugliestCodeArray[selectedPrizeIndex].code}
+            code={ugliestCodeArray[index].code}
           />
         )
       ))}
